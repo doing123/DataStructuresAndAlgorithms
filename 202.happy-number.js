abecdf1566843733018c8268792b8bddce00e37f
qinjunshan <13782533791@163.com>
2019-05-09 23:24:38 +0800

    '202.happy-number'

--- 202.happy-number.js
+++ 202.happy-number.js
@@ -8,15 +8,29 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-    while(n !=1 && n != 4) {
+    while(n != 1 && n != 4) {
         let result = 0
-        while(n % 10 > 0) {
+        while(n > 0) {
             let num = n % 10
-            n /= 10
+            n = parseInt(n / 10)
             result += num * num
         }
         n = result
     }
     return n === 1
+
+    // let set = new Set()
+    // while(n != 1) {
+    //     let sum = 0
+    //     while(n > 0) {
+    //         sum += (n % 10) * (n % 10)
+    //         n = parseInt(n / 10)
+    //     }
+    //     n = sum
+    //     if(set.has(n)) break
+    //     set.add(n)
+    // }
+
+    // return n === 1
 };
 
---
+++ 204.count-primes.js
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=204 lang=javascript
+ *
+ * [204] Count Primes
+ */
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    
+    
+};
+
