31e0c7c8388d847beba6f0cbb999e2c93a59c235
qinjunshan <13782533791@163.com>
2019-05-10 23:29:53 +0800

    '204:count-primes'

--- 204.count-primes.js
+++ 204.count-primes.js
@@ -8,7 +8,34 @@
  * @return {number}
  */
 var countPrimes = function(n) {
+    // let result = 0
+    // const arr = new Array(n).fill(true)
+
+    // for(let i = 2; i < n; i++) {
+    //     if(arr[i]) {
+    //         result++
+    //         for(let j = 2; i * j < n; j++) {
+    //             arr[i * j] = false
+    //         }
+    //     }
+    // }
+
+    // return result
+
+    let count = 0
+    const dict = []
+    const len = Math.floor(Math.sqrt(n))
+
+    for(let i = 2; i <= len; i++) {
+        for(let j = 2; i * j <= n; j++) {
+            dict[i * j] = true
+        }
+    }
     
-    
+    for(let i = 2; i < n; i++) {
+        if(!dict[i]) count++
+    }
+
+    return count
 };
 
---
+++ 231.power-of-two.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=231 lang=javascript
+ *
+ * [231] Power of Two
+ */
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    
+};
+
