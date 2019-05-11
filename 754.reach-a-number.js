7a47d8729219d52d7ec78023c7ba41396675b161
qinjunshan <13782533791@163.com>
2019-05-11 09:08:17 +0800

    344.reverse-string

---
+++ 754.reach-a-number.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=754 lang=javascript
+ *
+ * [754] Reach a Number
+ */
+/**
+ * @param {number} target
+ * @return {number}
+ */
+var reachNumber = function(target) {
+    
+};
+
