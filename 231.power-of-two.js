30d4a8c161bb4f826b243d00746253da69daf8d3
qinjunshan <13782533791@163.com>
2019-05-12 18:16:00 +0800

    '231.power-of-two'

--- 231.power-of-two.js
+++ 231.power-of-two.js
@@ -8,6 +8,11 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function(n) {
-    
+    if(n === 0) return false
+    while(n % 2 === 0) {
+        n = n / 2
+    }
+    if(n === 1) return true
+    return false
 };
 
