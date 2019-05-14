dd10773f1eab075afcb5bfa52d036b949cff250c
qinjunshan <13782533791@163.com>
2019-05-14 23:40:35 +0800

    '747:largest-number-at-least-twice-of-others'

--- 747.largest-number-at-least-twice-of-others.js
+++ 747.largest-number-at-least-twice-of-others.js
@@ -8,32 +8,38 @@
  * @return {number}
  */
 var dominantIndex = function(nums) {
-    // let largest = 0
-    // let index
-    // nums.forEach((item, i) => {
-    //     if(item > largest) {
-    //         largest = item
-    //         index = i
-    //     }
-    // })
-    // let flag = nums.find((item, i) => {
-    //     return index != i && item * 2 > largest
-    // })
-    // return flag ? -1 : index
-
-    let max = 0
-    let sencondMax = 0
+    let largest = 0
     let index
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] > max) {
-            sencondMax = max
-            max = nums[i]
+    nums.forEach((item, i) => {
+        if(item > largest) {
+            largest = item
             index = i
-        } else if(nums[i] > sencondMax) {
-            sencondMax = nums[i]
         }
-    }
+    })
+    /* let flag = nums.find((item, i) => {
+        return index != i && item * 2 > largest
+    })
+    return flag ? -1 : index */
 
-    return sencondMax * 2 > max ? -1 : index
+    for(let i = 0; i < nums.length; i++) {
+        if(index != i && item * 2 > largest) return -1
+    }
+    return index
+
+
+    // let max = 0
+    // let sencondMax = 0
+    // let index
+    // for(let i = 0; i < nums.length; i++) {
+    //     if(nums[i] > max) {
+    //         sencondMax = max
+    //         max = nums[i]
+    //         index = i
+    //     } else if(nums[i] > sencondMax) {
+    //         sencondMax = nums[i]
+    //     }
+    // }
+
+    // return sencondMax * 2 > max ? -1 : index
 };
 
