3f9b3585c4608122cc07ef32cb408d472079ebe7
qinjunshan <13782533791@163.com>
2019-05-14 23:35:22 +0800

    '747:largest-number-at-least-twice-of-others'

--- 747.largest-number-at-least-twice-of-others.js
+++ 747.largest-number-at-least-twice-of-others.js
@@ -8,17 +8,32 @@
  * @return {number}
  */
 var dominantIndex = function(nums) {
-    let largest = 0
+    // let largest = 0
+    // let index
+    // nums.forEach((item, i) => {
+    //     if(item > largest) {
+    //         largest = item
+    //         index = i
+    //     }
+    // })
+    // let flag = nums.find((item, i) => {
+    //     return index != i && item * 2 > largest
+    // })
+    // return flag ? -1 : index
+
+    let max = 0
+    let sencondMax = 0
     let index
-    nums.forEach((item, i) => {
-        if(item > largest) {
-            largest = item
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] > max) {
+            sencondMax = max
+            max = nums[i]
             index = i
+        } else if(nums[i] > sencondMax) {
+            sencondMax = nums[i]
         }
-    })
-    let flag = nums.find((item, i) => {
-        return index != i && item * 2 > largest
-    })
-    return flag ? -1 : index
+    }
+
+    return sencondMax * 2 > max ? index : -1
 };
 
