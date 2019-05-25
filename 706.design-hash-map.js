93ea861576098e757f4704082859effc312a419c
qinjunshan <13782533791@163.com>
2019-05-25 10:13:27 +0800

    706:design-hash-map

---
+++ 706.design-hash-map.js
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=706 lang=javascript
+ *
+ * [706] Design HashMap
+ */
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function () {
+  this.map = new Map()
+};
+
+/**
+ * value will always be non-negative. 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function (key, value) {
+  this.map.set(key, value)
+};
+
+/**
+ * Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function (key) {
+  const value = this.map.get(key)
+  return value !== undefined ? value : -1
+};
+
+/**
+ * Removes the mapping of the specified value key if this map contains a mapping for the key 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function (key) {
+  this.map.delete(key)
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
+
