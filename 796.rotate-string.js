b9f5732eb5ebf95c1ee245f97c17b936c689bed7
qinjunshan <13782533791@163.com>
2019-05-27 09:26:04 +0800

    796:rotate-string

--- 796.rotate-string.js
+++ 796.rotate-string.js
@@ -8,17 +8,20 @@
  * @param {string} B
  * @return {boolean}
  */
-var rotateString = function(A, B) {
+var rotateString = function (A, B) {
   // substr 从起始索引号提取字符串中指定数目的字符
   // substring 提取字符串中两个指定的索引号之间的字符
-  
-  if(A.length !== B.length) return false
+
+  /* if(A.length !== B.length) return false
   let i = 0
   while(i < A.length) {
     A = A.substring(1) + A.substr(0, 1)
     if(A === B) return true
     i++
   }
-  return A === B
+  return A === B */
+
+  // 2
+  return A.length == B.length && (A + A).indexOf(B) > -1
 };
 
