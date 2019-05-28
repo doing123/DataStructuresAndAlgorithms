d132c1e2ed0f4f2f46c10f69d178e3e59a50b555
qinjunshan <13782533791@163.com>
2019-05-28 09:15:05 +0800

    561"array-partition-i

---
+++ 561.array-partition-i.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=561 lang=javascript
+ *
+ * [561] Array Partition I
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    nums.sort((a, b) => a - b)
+    return nums.reduce((total, num, index) => {
+      return total + (index % 2 === 0 ? num : 0)
+    })
+};
+
