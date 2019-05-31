ee2146745aaf32ff2705a06d345007b5c146bf08
qinjunshan <13782533791@163.com>
2019-05-31 09:35:10 +0800

    896:monotonic-array

--- 896.monotonic-array.js
+++ 896.monotonic-array.js
@@ -8,13 +8,29 @@
  * @return {boolean}
  */
 var isMonotonic = function(A) {
-  let inc = true
+  /* let inc = true
   let dec = true
   for(let i = 0; i < A.length - 1; i++) {
     inc = inc && (A[i] <= A[i + 1])
     dec = dec && (A[i] >= A[i + 1])
   }
 
-  return inc || dec
+  return inc || dec */
+
+  return increasing(A) || decreasing(A)
 };
+
+function increasing(A) {
+  for(let i = 0; i < A.length - 1; i++) {
+    if(A[i] > A[i + 1]) return false
+  }
+  return true
+}
+
+function decreasing(A) {
+  for(let i = 0; i < A.length - 1; i++) {
+    if(A[i] < A[i + 1]) return false
+  }
+  return true
+}
 
