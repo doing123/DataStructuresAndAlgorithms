0e4609806dc9eac9bf3c098c4048804e7fd74d5c
qinjunshan <13782533791@163.com>
2019-06-04 10:01:47 +0800

    404:sum-of-left-leaves

--- 404.sum-of-left-leaves.js
+++ 404.sum-of-left-leaves.js
@@ -32,11 +32,45 @@ var sumOfLeftLeaves = function (root) {
     calculate(node.right, false)
   } */
 
-  if (!root) return 0
+  /* if (!root) return 0
   if (root.left && !root.left.left && !root.left.right) {
     return root.left.val + sumOfLeftLeaves(root.right)
   }
 
-  return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
+  return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right) */
+
+  /**
+   * 迭代实现：queue
+   */
+  /* if (!root || (!root.left && !root.right)) return 0
+  let result = 0
+  const queue = []
+  queue.push(root)
+
+  while (queue.length) {
+    const node = queue.shift()
+    if (node.left && !node.left.left && !node.left.right) result += node.left.val
+    if (node.left) queue.push(node.left)
+    if (node.right) queue.push(node.right)
+  }
+
+  return result */
+
+  /**
+   * 迭代实现：stack
+   */
+  if (!root || (!root.left && !root.right)) return 0
+  let result = 0
+  const stack = []
+  stack.push(root)
+
+  while (stack.length) {
+    const node = stack.pop()
+    if (node.left && !node.left.left && !node.left.right) result += node.left.val
+    if (node.left) stack.push(node.left)
+    if (node.right) stack.push(node.right)
+  }
+
+  return result
 };
 
