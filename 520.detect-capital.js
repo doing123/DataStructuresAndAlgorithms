022c341ad258018aa9237b94b0fc9f64632d5baa
qinjunshan <13782533791@163.com>
2019-06-05 09:49:34 +0800

    520:detect-capital

---
+++ 520.detect-capital.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=520 lang=javascript
+ *
+ * [520] Detect Capital
+ */
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+  let count = 0
+  let len = word.length
+  for(let i = 0; i < len; i++) {
+    if(word[i] <= 'Z') count++
+  }
+  return count == 0 || count == len || (count == 1 && word[0] <= 'Z')
+};
+
