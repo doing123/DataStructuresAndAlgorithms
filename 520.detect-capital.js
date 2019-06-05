dfa0da5cecc6a2657d77b92ad1107617114c024c
qinjunshan <13782533791@163.com>
2019-06-05 09:51:48 +0800

    520:detect-capital

--- 520.detect-capital.js
+++ 520.detect-capital.js
@@ -7,11 +7,11 @@
  * @param {string} word
  * @return {boolean}
  */
-var detectCapitalUse = function(word) {
+var detectCapitalUse = function (word) {
   let count = 0
   let len = word.length
-  for(let i = 0; i < len; i++) {
-    if(word[i] <= 'Z') count++
+  for (let i = 0; i < len; i++) {
+    if (word[i] <= 'Z') count++
   }
   return count == 0 || count == len || (count == 1 && word[0] <= 'Z')
 };
