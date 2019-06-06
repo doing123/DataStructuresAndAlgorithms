3aea685d3c09ee2f9fce13137bf2f481ee156684
qinjunshan <13782533791@163.com>
2019-06-06 10:00:03 +0800

    594:longest-harmonious-subsequence

--- 594.longest-harmonious-subsequence.js
+++ 594.longest-harmonious-subsequence.js
@@ -8,11 +8,13 @@
  * @return {number}
  */
 var findLHS = function (nums) {
-  nums.sort((a, b) => a - b)
-  let res = 0
+  /* let res = 0
   let map = new Map()
   for (let i = 0; i < nums.length; i++) {
     let key = nums[i]
+  // for (let key of nums) { // 效率：for...of < for...in < for 
+  // for (let i in nums) {
+  //   let key = nums[i]
     if (map.has(key)) {
       map.set(key, map.get(key) + 1)
     } else {
@@ -25,6 +27,59 @@ var findLHS = function (nums) {
     if (map.has(key - 1)) {
       res = Math.max(res, map.get(key) + map.get(key - 1))
     }
+  }
+
+  return res */
+
+  // 2
+  /* let res = 0
+  let map = {}
+  for(let i = 0; i < nums.length; i++) {
+    let key = nums[i]
+    if(map[key]) {
+      map[key]++
+    } else {
+      map[key] = 1
+    }
+    if(map[key + 1]) {
+      res = Math.max(res, map[key] + map[key + 1])
+    }
+    if(map[key - 1]) {
+      res = Math.max(res, map[key] + map[key - 1])
+    }
+  }
+
+  return res */
+
+  // 3
+  /* let res = 0
+  let map = {}
+  for(let i = 0; i < nums.length; i++) {
+    let key = nums[i]
+    if(map[key]) {
+      map[key]++
+    } else {
+      map[key] = 1
+    }
+  }
+  for(let key in map) {
+    key = Number(key) // object 的 key 是 字符串
+    if(map[key + 1]) {
+      res = Math.max(res, map[key] + map[key + 1])
+    }
+  }
+
+  return res */
+
+  // 4
+  let res = 0
+  let start = 0
+  let newStart = 0
+  nums.sort((a, b) => a - b)
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] - nums[start] > 1) start = newStart
+    if (nums[i] != nums[i - 1]) newStart = i
+    if (nums[i] - nums[start] == 1) res = Math.max(res, i - start + 1)
   }
 
   return res
