221127f4e78d91f0e7d8e2e41fa73d7c51f7f8d1
qinjunshan <13782533791@163.com>
2019-06-06 09:20:22 +0800

    594:longest-harmonious-subsequence

--- 594.longest-harmonious-subsequence.js
+++ 594.longest-harmonious-subsequence.js
@@ -7,22 +7,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findLHS = function(nums) {
+var findLHS = function (nums) {
   nums.sort((a, b) => a - b)
   let res = 0
   let map = new Map()
-  for(let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     let key = nums[i]
-    if(map.has(key)) {
+    if (map.has(key)) {
       map.set(key, map.get(key) + 1)
     } else {
       map.set(key, 1)
     }
 
-    if(map.has(key + 1)) {
+    if (map.has(key + 1)) {
       res = Math.max(res, map.get(key) + map.get(key + 1))
     }
-    if(map.has(key - 1)) {
+    if (map.has(key - 1)) {
       res = Math.max(res, map.get(key) + map.get(key - 1))
     }
   }
