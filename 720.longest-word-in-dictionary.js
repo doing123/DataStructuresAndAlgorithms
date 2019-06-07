4392ce31b716ff4d6e026e53bcae873dc9bb428f
qinjunshan <13782533791@163.com>
2019-06-07 19:00:25 +0800

    '720:longest-word-in-dictionary'

---
+++ 720.longest-word-in-dictionary.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=720 lang=javascript
+ *
+ * [720] Longest Word in Dictionary
+ */
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var longestWord = function(words) {
+    
+};
+
