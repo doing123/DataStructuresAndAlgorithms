d2ed16a166b01b0cd2c6dc640ecabe08bffcb9be
qinjunshan <13782533791@163.com>
2019-06-12 09:50:31 +0800

    696:count-binary-substrings

--- 696.count-binary-substrings.js
+++ 696.count-binary-substrings.js
@@ -8,7 +8,8 @@
  * @return {number}
  */
 var countBinarySubstrings = function (s) {
-  const res = []
+  // TODO: RegExp too big
+  /* const res = []
 
   for (let i = 0; i < s.length - 1; i++) {
     const sub = match(s.slice(i))
@@ -17,17 +18,50 @@ var countBinarySubstrings = function (s) {
     }
   }
 
-  return res.length
+  return res.length */
+
+  /* let zeros = 0
+  let ones = 0
+  let res = 0
+  for (let i = 0; i < s.length; i++) {
+    if (i == 0) {
+      (s[i] == '1') ? ones++ : zeros++
+    } else {
+      if (s[i] == '1') {
+        ones = (s[i - 1] == '1') ? ones + 1 : 1
+        if (zeros >= ones) res++
+      } else if (s[i] == '0') {
+        zeros = (s[i - 1] == '0') ? zeros + 1 : 1
+        if (ones >= zeros) res++
+      }
+    }
+  }
+
+  return res */
+
+  let res = 0
+  let pre = 0
+  let cur = 1
+  for (let i = 1; i < s.length; i++) {
+    if (s[i] == s[i - 1]) cur++
+    else {
+      pre = cur
+      cur = 1
+    }
+    if (pre >= cur) res++
+  }
+
+  return res
 };
 
-function match(str) {
+/* function match(str) {
   const j = str.match(/^(0+|1+)/)[0]
   const o = (j[0] ^ 1).toString().repeat(j.length)
-  const reg = new RegExp(`^${j}${o}`)
+  const reg = new RegExp(`^(${j}${o})`)
   if (reg.test(str)) {
     return RegExp.$1
   } else {
     return ''
   }
-}
+} */
 
