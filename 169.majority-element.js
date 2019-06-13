c6ddb356d3374ac1003e7dbbcab2471da55e1ef8
qinjunshan <13782533791@163.com>
2019-06-13 10:32:15 +0800

    169:majority-element

--- 169.majority-element.js
+++ 169.majority-element.js
@@ -8,7 +8,7 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-  let res = 0
+  /* let res = 0
   let count = 0
   for (let i = 0; i < nums.length; i++) {
     if (count === 0) {
@@ -16,6 +16,22 @@ var majorityElement = function (nums) {
     } else {
       nums[i] === res ? count++ : count--
     }
+  }
+
+  return res */
+
+  // 位操作 Bit Manipulation
+  let res = 0
+  let n = Math.floor(nums.length / 2)
+  for (let i = 0; i < 32; i++) {
+    let ones = 0
+    let zeros = 0
+    for (let j = 0; j < nums.length; j++) {
+      if (ones > n || zeros > n) break
+      if ((nums[j] & (1 << i)) !== 0) ones++
+      else zeros++
+    }
+    if (ones > zeros) res |= (1 << i)
   }
 
   return res
