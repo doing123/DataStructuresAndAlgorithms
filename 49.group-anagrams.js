3cbdc501017ef483783a39fd84e4c966e7d28696
qinjunshan <13782533791@163.com>
2019-06-17 13:34:51 +0800

    49:group-anagrams

--- 49.group-anagrams.js
+++ 49.group-anagrams.js
@@ -8,12 +8,34 @@
  * @return {string[][]}
  */
 var groupAnagrams = function (strs) {
-  let res = []
+  /* let res = []
   let map = {}
   for (let i = 0; i < strs.length; i++) {
     let str = strs[i].split('').sort().join('')
     if (!map[str]) map[str] = []
     map[str].push(strs[i])
+  }
+
+  for (let key in map) {
+    res.push(map[key])
+  }
+
+  return res */
+
+  let res = []
+  let map = {}
+  for (let i = 0; i < strs.length; i++) {
+    let arr = new Array(26).fill(0)
+    let str = strs[i]
+    let tmp = ''
+    for (let c = 0; c < str.length; c++) {
+      arr[str[c].charCodeAt() - 97]++
+    }
+    for (let j = 0; j < arr.length; j++) {
+      tmp += arr[j]
+    }
+    if (!map[tmp]) map[tmp] = []
+    map[tmp].push(str)
   }
 
   for (let key in map) {
