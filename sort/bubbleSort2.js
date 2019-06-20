e9d84a75b0ec5addbc4d64e62854eb4887a7c9b1
qinjunshan <13782533791@163.com>
2019-06-20 22:59:43 +0800

    'bubble-sort'

---
+++ sort/bubbleSort2.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @param {number[]} nums 
+ */
+function bubbleSort(nums) {
+  for (let i = nums.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+      }
+    }
+  }
+  
+  return nums
+}
