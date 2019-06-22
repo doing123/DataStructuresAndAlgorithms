f55fb52deafa94860aafaf56a9f1c6b212740256
qinjunshan <13782533791@163.com>
2019-06-22 19:42:49 +0800

    30:substring-with-concatenation-of-all-words

--- 30.substring-with-concatenation-of-all-words.js
+++ 30.substring-with-concatenation-of-all-words.js
@@ -9,5 +9,22 @@
  * @return {number[]}
  */
 var findSubstring = function (s, words) {
+  let result = []
+  let len = words.length
+  let recursive = (r, _arr) => {
+    if (r.length === len) {
+      result.push(r)
+    } else {
+      _arr.forEach((item, index) => {
+        let tmp = [].concat(_arr)
+        tmp.splice(index, 1)
+        recursive(r.concat(item), tmp)
+      })
+    }
+  }
+  recursive([], words)
 
+  return result.map(item => {
+    return s.indexOf(item.join(''))
+  }).filter(item => item !== -1).sort()
 }
