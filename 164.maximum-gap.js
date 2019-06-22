ce377df45ca6d335cd5eccd00730e5e40a772b41
qinjunshan <13782533791@163.com>
2019-06-22 16:54:17 +0800

    164:maximum-map

--- 164.maximum-gap.js
+++ 164.maximum-gap.js
@@ -8,9 +8,10 @@
  * @return {number}
  */
 var maximumGap = function (nums) {
+  if (nums.length < 2) return 0
   let max = 0
   nums.sort((a, b) => a - b)
-  for (let i = 0; i < nums.length - 1; i++) {
+  for (let i = 0, len = nums.length - 1; i < len; i++) {
     max = Math.max(max, nums[i + 1] - nums[i])
   }
 
