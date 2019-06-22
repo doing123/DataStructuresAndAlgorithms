4a473bd4aea8ab0af236dad59d8df620b4118e09
qinjunshan <13782533791@163.com>
2019-06-22 19:24:18 +0800

    93:restore-ip-address

---
+++ 30.substring-with-concatenation-of-all-words.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=30 lang=javascript
+ *
+ * [30] Substring with Concatenation of All Words
+ */
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+var findSubstring = function (s, words) {
+
+}
--- 93.restore-ip-addresses.js
+++ 93.restore-ip-addresses.js
@@ -15,6 +15,10 @@ var restoreIpAddresses = function (s) {
     } else {
       for (let i = 0, len = Math.min(3, str.length); i < len; i++) {
         let tmp = str.substr(0, i + 1)
+        // 当只有一位时，0可以成某一段，如果有两位或三位时，像 00， 01， 001， 011， 000等都是不合法的
+        if (tmp.length > 1 && tmp[0] === '0') { // '0'
+          continue
+        }
         if (tmp < 256) {
           recursive(arr.concat([tmp]), str.substr(i + 1))
         }
