9df2f35d410a7ca0dfb596a02eb5392cfda3e91b
qinjunshan <13782533791@163.com>
2019-06-22 17:08:33 +0800

    164:maximum-map

--- 164.maximum-gap.js
+++ 164.maximum-gap.js
@@ -10,10 +10,25 @@
 var maximumGap = function (nums) {
   if (nums.length < 2) return 0
   let max = 0
-  nums.sort((a, b) => a - b)
+  // 1
+  /* nums.sort((a, b) => a - b)
   for (let i = 0, len = nums.length - 1; i < len; i++) {
     max = Math.max(max, nums[i + 1] - nums[i])
   }
 
-  return max
+  return max */
+
+  // 2
+  let len = nums.length - 1
+  for (let i = len; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+      }
+    }
+    if (i < len) {
+      max = Math.max(max, nums[i + 1] - nums[i])
+    }
+  }
+  return Math.max(max, nums[1] - nums[0])
 }
--- 215.kth-largest-element-in-an-array.js
+++ 215.kth-largest-element-in-an-array.js
@@ -15,7 +15,7 @@ var findKthLargest = function (nums, k) {
   // return nums[nums.length - k]
 
   let len = nums.length - 1
-  for (let i = len; i >= 0; i--) {
+  for (let i = len; i > 0; i--) {
     for (let j = 0; j < i; j++) {
       if (nums[j] > nums[j + 1]) {
         [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
