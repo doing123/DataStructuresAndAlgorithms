9d6a5be67b0bd324a7bdb660caffe757c6b65215
qinjunshan <13782533791@163.com>
2019-06-22 16:39:15 +0800

    215:kth-largest-element-in-an-array

--- 215.kth-largest-element-in-an-array.js
+++ 215.kth-largest-element-in-an-array.js
@@ -9,6 +9,20 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-  nums.sort((a, b) => a - b)
-  return nums[nums.length - k]
+  // return nums.sort((a, b) => b - a)[k - 1]
+
+  // nums.sort((a, b) => a - b)
+  // return nums[nums.length - k]
+
+  let len = nums.length - 1
+  for (let i = len; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]]
+      }
+    }
+    if (i === len - (k - 1)) {
+      return nums[i]
+    }
+  }
 }
