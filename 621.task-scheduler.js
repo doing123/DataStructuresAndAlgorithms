a297b9635b79c5063caa954fd3e9c5ae2daf8cc6
qinjunshan <13782533791@163.com>
2019-07-04 10:13:24 +0800

    621:task-scheduler

--- 621.task-scheduler.js
+++ 621.task-scheduler.js
@@ -9,7 +9,8 @@
  * @return {number}
  */
 var leastInterval = function (tasks, n) {
-  let arr = Array(26).fill(0)
+  // 1.
+  /* let arr = Array(26).fill(0)
   for (let i = 0; i < tasks.length; i++) {
     // 'A'.charCodeAt() // 65
     arr[tasks[i].charCodeAt() - 65]++
@@ -19,5 +20,26 @@ var leastInterval = function (tasks, n) {
   let mx = arr[25]
   let len = tasks.length
   while (i >= 0 && arr[i] === mx) i--
-  return Math.max(len, (mx - 1) * (n + 1) + 25 - i)
+  return Math.max(len, (mx - 1) * (n + 1) + 25 - i) */
+
+  // 2.
+  let mx = 0
+  let mxCnt = 0
+  let arr = Array(26).fill(0)
+  for (let i = 0; i < tasks.length; i++) {
+    arr[tasks[i].charCodeAt() - 65]++
+    let tmp = arr[tasks[i].charCodeAt() - 65]
+    if (mx === tmp) {
+      mxCnt++
+    } else if (mx < tmp) {
+      mx = tmp
+      mxCnt = 1
+    }
+  }
+  let partCnt = mx - 1
+  let partLen = n - (mxCnt - 1)
+  let emptySlots = partCnt * partLen
+  let taskLeft = tasks.length - mx * mxCnt
+  let idles = Math.max(0, emptySlots - taskLeft)
+  return tasks.length + idles
 }
