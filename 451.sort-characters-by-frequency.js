f1d439faefe48ed8498b3ebbbc7596d644b81f67
qinjunshan <13782533791@163.com>
2019-07-11 10:08:00 +0800

    451:sort-characters-by-frequency

--- 451.sort-characters-by-frequency.js
+++ 451.sort-characters-by-frequency.js
@@ -8,7 +8,8 @@
  * @return {string}
  */
 var frequencySort = function (s) {
-  let map = {}
+  // 1.map映射，values数组排序，再查找
+  /* let map = {}
   for (let i = 0; i < s.length; i++) {
     let str = s[i]
     if (map[str]) {
@@ -24,12 +25,92 @@ var frequencySort = function (s) {
     Object.keys(map).forEach(k => {
       if (map[k] === item) {
         result += k.repeat(item)
-        delete map[k]
+        // delete map[k]
+        this.map.delete(k)
       }
     })
   })
 
-  return result
+  return result */
+
+  // 2.TODO:堆排序
+  // Test:不通过  Actual: eetr  Expected: eert
+  // Submit 又是成功的。神奇了
+  let heap = new Heap(s)
+  return heap.toString()
 }
 
-frequencySort('tree')
+// 定义 heap
+class Heap {
+  constructor (str) {
+    let map = new Map()
+    for (let i = 0; i < str.length; i++) {
+      let tmp = str[i]
+      if (map.has(tmp)) {
+        map.set(tmp, map.get(tmp) + 1)
+      } else {
+        map.set(tmp, 1)
+      }
+    }
+    this.map = map
+    this.data = Array.from(map.values())
+  }
+  sort () {
+    let iArr = this.data
+    let n = iArr.length
+    if (n <= 1) {
+      return iArr
+    } else {
+      for (let i = Math.floor(n / 2); i >= 0; i--) {
+        Heap.maxHeapify(iArr, i, n)
+      }
+      for (let j = 0; j < n; j++) {
+        Heap.swap(iArr, 0, n - 1 - j)
+        Heap.maxHeapify(iArr, 0, n - 1 - j - 1)
+      }
+      return iArr
+    }
+  }
+  toString () {
+    let arr = this.sort()
+    let str = []
+    while (arr.length) {
+      let top = arr.pop()
+      for (let [k, v] of this.map) {
+        if (v === top) {
+          str.push(k.repeat(v))
+          this.map.delete(k)
+          break
+        }
+      }
+    }
+    return str.join('')
+  }
+  // 交换两个元素
+  static swap (arr, a, b) {
+    if (a === b) {
+      return
+    }
+    [arr[a], arr[b]] = [arr[b], arr[a]]
+  }
+  // 构建最大堆的过程
+  static maxHeapify (Arr, i, size) {
+    // 左节点（索引）
+    let l = i * 2 + 1
+    // 右节点
+    let r = i * 2 + 2
+    let largest = i
+    // 父节点 i 和左节点 l 做比较取最大
+    if (l <= size && Arr[l] > Arr[largest]) {
+      largest = l
+    }
+    // 右节点和最大值比较
+    if (r <= size && Arr[r] > Arr[largest]) {
+      largest = r
+    }
+    if (largest !== i) {
+      Heap.swap(Arr, i, largest)
+      Heap.maxHeapify(Arr, largest, size)
+    }
+  }
+}
