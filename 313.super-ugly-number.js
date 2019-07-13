5e4dd9a5b14b5e8d2aea2e220db215b48e943247
qinjunshan <13782533791@163.com>
2019-07-13 09:20:17 +0800

    313:super-ugly-number

---
+++ 313.super-ugly-number.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=313 lang=javascript
+ *
+ * [313] Super Ugly Number
+ */
+/**
+ * @param {number} n
+ * @param {number[]} primes
+ * @return {number}
+ */
+var nthSuperUglyNumber = function (n, primes) {
+  // TODO:
+}
