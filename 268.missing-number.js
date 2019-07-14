c87ee67ac4fd6493118fb7103717724f04f6fd75
qinjunshan <13782533791@163.com>
2019-07-14 23:14:50 +0800

    268:missing-number

--- 268.missing-number.js
+++ 268.missing-number.js
@@ -9,8 +9,13 @@
  */
 var missingNumber = function (nums) {
   let len = nums.length
-  let res = len
-  for (let i = 0; i < len; i++) {
+  // 利用位运算：异或XOR运算， X^X=0, X^0=X
+  // 这里nums中的最大值为 len = nums.length, 而遍历到 len - 1, 所以默认res 为 len
+  // 当然可以 res = 0， 遍历到 <= len
+  let res = 0
+  for (let i = 0; i <= len; i++) {
+  // let res = len
+  // for (let i = 0; i < len; i++) {
     res ^= nums[i] ^ i
   }
   return res
