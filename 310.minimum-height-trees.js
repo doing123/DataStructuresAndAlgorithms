b9e76bbeed90b3d27246f5e5d0db8d2edccc5e21
qinjunshan <13782533791@163.com>
2019-07-17 10:37:59 +0800

    310:minimum-height-trees

--- 310.minimum-height-trees.js
+++ 310.minimum-height-trees.js
@@ -11,9 +11,9 @@
 var findMinHeightTrees = function (n, edges) {
   if (n === 1) return [0]
   let res = []
-  let adj = new Array(n)
+  let adj = new Array(n) // 建立图，二维数组 adj
   for (let i = 0; i < adj.length; i++) {
-    adj[i] = []
+    adj[i] = [] // 保存了 i 节点可以到达的所有节点
   }
   let queue = []
   for (let i = 0; i < edges.length; i++) {
@@ -24,13 +24,16 @@ var findMinHeightTrees = function (n, edges) {
 
   for (let i = 0; i < n; i++) {
     if (adj[i].length === 1) {
-      queue.push(i)
+      queue.push(i) // 将所有只有一个连接边的节点（叶节点）都存入到一个队列queue中
     }
   }
 
   while (n > 2) {
     let size = queue.length
     n -= size
+    // 遍历每一个叶节点，通过图来找到和其相连的节点，并且在其相连节点的集合中将该叶节点删去，
+    // 如果删完后此节点也也变成一个叶节点了，加入队列中，再下一轮删除
+    // 节点数小于等于2时候停止，此时剩下的一个或两个节点就是我们要求的最小高度树的根节点
     for (let i = 0; i < size; i++) {
       let top = queue.shift()
       for (let j = 0; j < adj[top].length; j++) {
