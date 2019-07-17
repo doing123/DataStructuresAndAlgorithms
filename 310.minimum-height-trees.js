6e8bbf13b685325dc03962601a23298974d780c3
qinjunshan <13782533791@163.com>
2019-07-17 10:31:33 +0800

    310:minimum-height-trees

--- 310.minimum-height-trees.js
+++ 310.minimum-height-trees.js
@@ -9,5 +9,46 @@
  * @return {number[]}
  */
 var findMinHeightTrees = function (n, edges) {
-  // TODO:
+  if (n === 1) return [0]
+  let res = []
+  let adj = new Array(n)
+  for (let i = 0; i < adj.length; i++) {
+    adj[i] = []
+  }
+  let queue = []
+  for (let i = 0; i < edges.length; i++) {
+    let edge = edges[i]
+    adj[edge[0]].push(edge[1])
+    adj[edge[1]].push(edge[0])
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (adj[i].length === 1) {
+      queue.push(i)
+    }
+  }
+
+  while (n > 2) {
+    let size = queue.length
+    n -= size
+    for (let i = 0; i < size; i++) {
+      let top = queue.shift()
+      for (let j = 0; j < adj[top].length; j++) {
+        let tmp = adj[top][j]
+        let index = adj[tmp].findIndex(item => {
+          return item === top
+        })
+        adj[tmp].splice(index, 1)
+        if (adj[tmp].length === 1) queue.push(tmp)
+      }
+    }
+  }
+
+  while (queue.length) {
+    res.push(queue.shift())
+  }
+
+  return res
 }
+
+findMinHeightTrees(4, [[1, 0], [1, 2], [1, 3]])
