04e7aa578ffbdca88253728d657c22432c7045df
qinjunshan <13782533791@163.com>
2019-07-18 09:47:57 +0800

    4.median-of-two-sorted-arrays

--- 4.median-of-two-sorted-arrays.js
+++ 4.median-of-two-sorted-arrays.js
@@ -9,5 +9,16 @@
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  // TODO:待完成
+  // 1. concat 数组 排序
+  let arr = nums1.concat(nums2)
+  arr.sort((a, b) => a - b)
+  if (arr.length % 2 === 0) {
+    let tmp = arr.length / 2
+    return (arr[tmp - 1] + arr[tmp]) / 2
+  } else {
+    let tmp = Math.floor(arr.length / 2)
+    return arr[tmp]
+  }
+
+  // let max = Math.floor((nums1[nums1.length - 1] + nums2[nums2.length - 1]) / 2)
 }
