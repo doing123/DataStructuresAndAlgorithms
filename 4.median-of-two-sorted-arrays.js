81eba26eb159cd05c37f4680048d4b461d540633
qinjunshan <13782533791@163.com>
2019-07-18 10:14:30 +0800

    4.median-of-two-sorted-arrays

--- 4.median-of-two-sorted-arrays.js
+++ 4.median-of-two-sorted-arrays.js
@@ -10,7 +10,7 @@
  */
 var findMedianSortedArrays = function (nums1, nums2) {
   // 1. concat 数组 排序
-  let arr = nums1.concat(nums2)
+  /* let arr = nums1.concat(nums2)
   arr.sort((a, b) => a - b)
   if (arr.length % 2 === 0) {
     let tmp = arr.length / 2
@@ -18,7 +18,35 @@ var findMedianSortedArrays = function (nums1, nums2) {
   } else {
     let tmp = Math.floor(arr.length / 2)
     return arr[tmp]
-  }
+  } */
 
-  // let max = Math.floor((nums1[nums1.length - 1] + nums2[nums2.length - 1]) / 2)
+  // 2.count 计数
+  let total = nums1.length + nums2.length
+  let len = Math.floor(total / 2)
+  let flag = total % 2 === 0
+  let count = 0
+  let result = []
+  while (count <= len) {
+    if (!nums1.length && !nums2.length) {
+      return 0
+    } else if (!nums1.length && nums2.length) {
+      result.push(nums2.shift())
+    } else if (!nums2.length && nums1.length) {
+      result.push(nums1.shift())
+    } else {
+      if (nums1[0] < nums2[0]) {
+        result.push(nums1.shift())
+      } else {
+        result.push(nums2.shift())
+      }
+    }
+    count++
+  }
+  if (flag) {
+    return (result.pop() + result.pop()) / 2
+  } else {
+    return result.pop()
+  }
 }
+
+// findMedianSortedArrays([], [1])
