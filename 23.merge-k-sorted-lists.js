de72159c5528d76a81a8fc34a79b4309d9226105
qinjunshan <13782533791@163.com>
2019-07-20 22:59:11 +0800

    23:merge-k-sorted-lists

---
+++ 23.merge-k-sorted-lists.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=23 lang=javascript
+ *
+ * [23] Merge k Sorted Lists
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+  // TODO:
+}
