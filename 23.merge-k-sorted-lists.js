d1f5fb48bd7e5b0b90a3f3caeac3984ffc5975f9
qinjunshan <13782533791@163.com>
2019-07-21 10:22:22 +0800

    23:merge-k-sorted-lists

--- 23.merge-k-sorted-lists.js
+++ 23.merge-k-sorted-lists.js
@@ -16,4 +16,27 @@
  */
 var mergeKLists = function (lists) {
   // TODO:
+  let queue = []
+  for (let i = 0; i < lists.length; i++) {
+    if (lists[i]) {
+      queue.push(lists[i])
+    }
+  }
+  queue.sort((a, b) => a.val - b.val)
+
+  let dummy = new ListNode(0)
+  let cur = dummy
+  let tmp = null
+  while (queue.length) {
+    tmp = queue.shift()
+    cur.next = tmp
+    cur = cur.next
+    if (tmp.next !== null) {
+      queue.push(tmp.next)
+      queue.sort((a, b) => a.val - b.val)
+    }
+  }
+  cur = dummy.next
+  dummy = null
+  return cur
 }
