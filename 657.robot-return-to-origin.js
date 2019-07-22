094f61ce5c1798f6c26581a03f59a2efce206e7c
qinjunshan <13782533791@163.com>
2019-07-22 08:16:24 +0800

    657:robot-return-to-origin

--- 657.robot-return-to-origin.js
+++ 657.robot-return-to-origin.js
@@ -9,7 +9,7 @@
  */
 var judgeCircle = function (moves) {
   // 1.上下左右四个方向
-  let obj = {
+  /* let obj = {
     'L': 0,
     'R': 0,
     'D': 0,
@@ -18,5 +18,27 @@ var judgeCircle = function (moves) {
   for (let i = 0, len = moves.length; i < len; i++) {
     obj[moves[i]]++
   }
-  return obj['L'] === obj['R'] && obj['U'] === obj['D']
+  return obj['L'] === obj['R'] && obj['U'] === obj['D'] */
+
+  // 2.水平和垂直方向
+  let hDirection = 0 // horizontal direction
+  let vDirection = 0 // vertical direction
+  for (let i = 0, len = moves.length; i < len; i++) {
+    switch (moves.charAt(i)) {
+      case 'U':
+        vDirection++
+        break
+      case 'D':
+        vDirection--
+        break
+      case 'L':
+        hDirection--
+        break
+      default:
+        hDirection++
+        break
+    }
+  }
+
+  return vDirection === 0 && hDirection === 0
 }
