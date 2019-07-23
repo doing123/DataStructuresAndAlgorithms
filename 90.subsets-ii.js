d8e3f801ef9568467c2c4d160d6c5a9fb63e48ce
qinjunshan <13782533791@163.com>
2019-07-23 09:48:29 +0800

    90:subsets-ii

--- 90.subsets-ii.js
+++ 90.subsets-ii.js
@@ -7,7 +7,28 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var subsetsWithDup = function(nums) {
-    // TODO:
-};
+var subsetsWithDup = function (nums) {
+  if (!nums.length) return []
+  let res = [[]]
+  nums.sort((a, b) => a - b)
+  let size = 1
+  let last = nums[0]
+  for (let i = 0; i < nums.length; i++) {
+    if (last !== nums[i]) {
+      last = nums[i]
+      size = res.length
+    }
+    let newSize = res.length
+    for (let j = newSize - size; j < newSize; j++) {
+      let arr = JSON.parse(JSON.stringify(res[j]))
+      arr.push(nums[i])
+      res.push(arr)
+    }
+  }
 
+  return res // res.sort()
+
+  // TODO:递归，构建树
+}
+
+// subsetsWithDup([1, 2, 2])
