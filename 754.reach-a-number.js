a6680c0d7fa4cf40859925b40e37871354182a1e
qinjunshan <13782533791@163.com>
2019-07-23 09:01:18 +0800

    754:reach-a-number

--- 754.reach-a-number.js
+++ 754.reach-a-number.js
@@ -9,4 +9,12 @@
  */
 var reachNumber = function (target) {
   // TODO:
+  target = Math.abs(target)
+  // 求出第n步，使得从1累加到n刚好大于等于 target，利用求和公式 ???
+  let n = Math.ceil((-1 + Math.sqrt(1 + 8 * target)) / 2)
+  let sum = n * (n + 1) / 2
+  if (sum === target) return n
+  let res = sum - target
+  if ((res & 1) === 0) return n // 差值为偶数
+  return n + ((n & 1) ? 2 : 1) // 如果是奇数，判断此时n的奇偶，如果n是奇数，则返回 n+2，若n是偶数，返回 n+1
 }
