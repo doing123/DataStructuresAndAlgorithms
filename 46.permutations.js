e83a0064add407796d4919f2a79c4ec6df235bf2
qinjunshan <13782533791@163.com>
2019-07-25 09:30:07 +0800

    46:permutations

---
+++ 46.permutations.js
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=46 lang=javascript
+ *
+ * [46] Permutations
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  // TODO:
+}
