44085942739dc031c17c43a125845944260a07b6
qinjunshan <13782533791@163.com>
2019-07-26 09:21:34 +0800

    46:permutations

--- 46.permutations.js
+++ 46.permutations.js
@@ -8,5 +8,20 @@
  * @return {number[][]}
  */
 var permute = function (nums) {
-  // TODO:
+  let result = []
+  fullArrange(nums, nums.length, 0, result)
+  return result
+
+  function fullArrange (nums, size, n, result) {
+    if (n === size) {
+      result.push(nums)
+    }
+    for (let i = n; i < size; i++) {
+      [nums[i], nums[n]] = [nums[n], nums[i]]
+
+      fullArrange(nums.concat(), size, n + 1, result);
+
+      [nums[i], nums[n]] = [nums[n], nums[i]]
+    }
+  }
 }
