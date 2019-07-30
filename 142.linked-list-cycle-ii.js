2c6b9b1cb2f063bd50fb92e846dc6f318f2fc564
qinjunshan <13782533791@163.com>
2019-07-30 09:16:16 +0800

    142:linked-list-cycle-ii

--- 142.linked-list-cycle-ii.js
+++ 142.linked-list-cycle-ii.js
@@ -16,5 +16,24 @@
  * @return {ListNode}
  */
 var detectCycle = function (head) {
-  // TODO:
+  if (!head) return null
+  let fast = head
+  let slow = head
+
+  // 在fast和slow指针相遇后，fast指针不动，slow指针回到head，然后slow指针和fast指针同时向前走，
+  // 只不过这一次两个指针都是一步一步向前走。两个指针相遇的节点就是环路的起点。
+  while (fast.next !== null && fast.next.next !== null) {
+    slow = slow.next
+    fast = fast.next.next
+    if (fast === slow) {
+      slow = head
+      while (slow !== fast) {
+        slow = slow.next
+        fast = fast.next
+      }
+      return slow
+    }
+  }
+
+  return null
 }
