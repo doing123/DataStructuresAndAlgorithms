9e5feedab70c96cd9fc33cb41597bfb57af1e3a8
qinjunshan <13782533791@163.com>
2019-07-30 09:37:50 +0800

    160:intersection-of-two-linked-lists

--- 160.intersection-of-two-linked-lists.js
+++ 160.intersection-of-two-linked-lists.js
@@ -18,4 +18,19 @@
  */
 var getIntersectionNode = function (headA, headB) {
   // TODO:
+  let a = headA
+  let b = headB
+  while (a !== b) {
+    if (a === null) {
+      a = headB
+    } else {
+      a = a.next
+    }
+    if (b === null) {
+      b = headA
+    } else {
+      b = b.next
+    }
+  }
+  return a
 }
