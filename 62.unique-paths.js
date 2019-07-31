bccdf2d2c5d711b402c202162b5a784d4b5ae6eb
qinjunshan <13782533791@163.com>
2019-07-31 08:55:04 +0800

    62:unique-paths

--- 62.unique-paths.js
+++ 62.unique-paths.js
@@ -9,5 +9,15 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
-  // TODO:
+  // 动态规划：Dynamic Programming
+  // 二维数组：dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+  // 一维数组，一行行的刷新
+  let dp = Array(n).fill(1)
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[j] += dp[j - 1]
+    }
+  }
+
+  return dp[n - 1]
 }
