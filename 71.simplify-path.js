24def63af570d535c2be50e562068fbc4129b530
qinjunshan <13782533791@163.com>
2019-08-04 23:47:13 +0800

    todo

---
+++ 71.simplify-path.js
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=71 lang=javascript
+ *
+ * [71] Simplify Path
+ */
+/**
+ * @param {string} path
+ * @return {string}
+ */
+var simplifyPath = function (path) {
+  // TODO:
+}
---
+++ 86.partition-list.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=86 lang=javascript
+ *
+ * [86] Partition List
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} x
+ * @return {ListNode}
+ */
+var partition = function (head, x) {
+  // TODO:
+}
