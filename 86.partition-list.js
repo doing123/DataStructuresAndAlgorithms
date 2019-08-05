4db6006e7dcf77d110a0bba2fd7fc5da2d0f6e7a
qinjunshan <13782533791@163.com>
2019-08-05 09:41:08 +0800

    86:partition-list

--- 86.partition-list.js
+++ 86.partition-list.js
@@ -16,8 +16,8 @@
  * @return {ListNode}
  */
 var partition = function (head, x) {
-  // 1.局部排序
-  let dummy = new ListNode(-1)
+  // 1.划分链表，把所有小于给定值的节点都移到前面，大于该值的节点顺序不变，相当于局部排序
+  /* let dummy = new ListNode(-1)
   dummy.next = head
   let pre = dummy
   let cur = head
@@ -35,5 +35,25 @@ var partition = function (head, x) {
       cur = cur.next
     }
   }
-  return dummy.next
+  return dummy.next */
+
+  // 2.将所有小于给定值的节点取出组成一个新的链表，此时原链表中剩余的节点的值都大于或等于给定值，只要将原链表直接接在新链表后
+  if (!head) return head
+  let dummy = new ListNode(-1)
+  let newDummy = new ListNode(-1)
+  dummy.next = head
+  let cur = dummy
+  let pre = newDummy
+  while (cur.next) {
+    if (cur.next.val < x) {
+      pre.next = cur.next
+      pre = pre.next
+      cur.next = cur.next.next
+      pre.next = null
+    } else {
+      cur = cur.next
+    }
+  }
+  pre.next = dummy.next
+  return newDummy.next
 }
