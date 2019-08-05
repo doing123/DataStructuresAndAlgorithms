764fcff607abccd6d405ff7da43027cf9b7212f0
qinjunshan <13782533791@163.com>
2019-08-05 09:32:29 +0800

    86:partition-list

--- 86.partition-list.js
+++ 86.partition-list.js
@@ -16,5 +16,24 @@
  * @return {ListNode}
  */
 var partition = function (head, x) {
-  // TODO:
+  // 1.局部排序
+  let dummy = new ListNode(-1)
+  dummy.next = head
+  let pre = dummy
+  let cur = head
+  // 首先找到第一个大于或等于给定值的节点
+  while (pre.next && pre.next.val < x) pre = pre.next
+  cur = pre
+  while (cur.next) {
+    if (cur.next.val < x) {
+      let tmp = cur.next
+      cur.next = tmp.next
+      tmp.next = pre.next
+      pre.next = tmp
+      pre = pre.next
+    } else {
+      cur = cur.next
+    }
+  }
+  return dummy.next
 }
