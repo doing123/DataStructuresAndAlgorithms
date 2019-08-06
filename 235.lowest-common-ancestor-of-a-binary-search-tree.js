5ca11b2bc7b8241c5694782906b61ef3f24b0b3a
qinjunshan <13782533791@163.com>
2019-08-06 10:01:58 +0800

    235:lowest-common-ancestor-of-a-binary-search-tree

--- 235.lowest-common-ancestor-of-a-binary-search-tree.js
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.js
@@ -18,7 +18,7 @@
  */
 var lowestCommonAncestor = function (root, p, q) {
   // 1.递归：二叉搜索树的最小共同父节点
-  // 二叉搜索树的特点是左<根<右，所以根节点的值一直都是中间值，大于左子树的所有节点值，小于右子树的所有节点值
+  /* // 二叉搜索树的特点是左<根<右，所以根节点的值一直都是中间值，大于左子树的所有节点值，小于右子树的所有节点值
   if (!root) return null
   // 如果根节点的值大于p和q之间的较大值，说明p和q都在左子树中，那么此时我们就进入根节点的左子节点继续递归
   // 如果根节点小于p和q之间的较小值，说明p和q都在右子树中，那么此时我们就进入根节点的右子节点继续递归，
@@ -29,5 +29,17 @@ var lowestCommonAncestor = function (root, p, q) {
     return lowestCommonAncestor(root.right, p, q)
   } else {
     return root
+  } */
+
+  // 2.while循环代替递归
+  while (true) {
+    if (root.val > Math.max(p.val, q.val)) {
+      root = root.left
+    } else if (root.val < Math.min(p.val, q.val)) {
+      root = root.right
+    } else {
+      break
+    }
   }
+  return root
 }
