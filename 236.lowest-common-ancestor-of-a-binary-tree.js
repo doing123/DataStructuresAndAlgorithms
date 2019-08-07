9c2bafa0216ee9ba214200ed1a518a6465d2cc95
qinjunshan <13782533791@163.com>
2019-08-07 08:58:25 +0800

    236:lowest-common-ancestor-of-a-binary-tree

--- 236.lowest-common-ancestor-of-a-binary-tree.js
+++ 236.lowest-common-ancestor-of-a-binary-tree.js
@@ -18,4 +18,13 @@
  */
 var lowestCommonAncestor = function (root, p, q) {
   // TODO: 237,238,292
+  if (root === null) return null
+  if (root === p) return p
+  if (root === q) return q
+
+  let left = lowestCommonAncestor(root.left, p, q)
+  let right = lowestCommonAncestor(root.right, p, q)
+
+  if (left !== null && right !== null) return root
+  return left !== null ? left : right
 }
