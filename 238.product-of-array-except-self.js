0ad98918c4251d81f31278dece2946a08e3a082d
qinjunshan <13782533791@163.com>
2019-08-07 09:55:48 +0800

    238:product-of-array-except-self

--- 238.product-of-array-except-self.js
+++ 238.product-of-array-except-self.js
@@ -8,7 +8,8 @@
  * @return {number[]}
  */
 var productExceptSelf = function (nums) {
-  let result = []
+  // 1.求乘积
+  /* let result = []
   let tmp
   for (let i = 0; i < nums.length; i++) {
     // tmp = nums.slice()
@@ -16,8 +17,27 @@ var productExceptSelf = function (nums) {
     tmp = nums.slice(0, i).concat(nums.slice(i + 1))
     result.push(getTotal(tmp))
   }
-  return result
+  return result */
+
+  // 2.其前面所有数字的乘积，同时也知道后面所有的数乘积，二者相乘
+  let len = nums.length
+  let head = Array(len).fill(1)
+  let tail = Array(len).fill(1)
+  let res = Array(len)
+  for (let i = 0; i < len - 1; i++) {
+    head[i + 1] = head[i] * nums[i]
+  }
+  for (let i = len - 1; i > 0; i--) {
+    tail[i - 1] = tail[i] * nums[i]
+  }
+  for (let i = 0; i < len; i++) {
+    res[i] = head[i] * tail[i]
+  }
+
+  return res
 }
+
+// productExceptSelf([1, 2, 3, 4])
 
 function getTotal (arr) {
   return arr.reduce((total, item) => {
