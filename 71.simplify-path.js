01aa19ec3c7c610321057f8ff12c45a7f6d4c3c1
qinjunshan <13782533791@163.com>
2019-08-08 09:17:21 +0800

    71:simplify-path

--- 71.simplify-path.js
+++ 71.simplify-path.js
@@ -8,7 +8,8 @@
  * @return {string}
  */
 var simplifyPath = function (path) {
-  let stack = []
+  // 1.利用栈：/ 分割成数组
+  /* let stack = []
   let arr = path.split('/')
   for (let i = 0; i < arr.length; i++) {
     let tmp = arr[i]
@@ -18,5 +19,30 @@ var simplifyPath = function (path) {
       stack.push(tmp)
     }
   }
-  return `/${stack.join('/')}`
+  return `/${stack.join('/')}` */
+
+  // 2.单指针遍历
+  let stack = []
+  let i = 0
+  while (i < path.length) {
+    while (path[i] === '/' && i < path.length) {
+      i++
+    }
+    if (i === path.length) break
+
+    let start = i
+    while (path[i] !== '/' && i < path.length) {
+      i++
+    }
+    let end = i
+    // substring：提取两个指定索引之间的字符。 同 slice
+    // substr：从起始索引提取指定数目的字符
+    let str = path.substring(start, end)
+    if (str === '..') {
+      if (stack.length) stack.pop()
+    } else if (str !== '.') {
+      stack.push(str)
+    }
+  }
+  return '/' + stack.join('/')
 }
