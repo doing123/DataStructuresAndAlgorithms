1ee4ba01e50d6286ee18cf52535a22d0ebc327a9
qinjunshan <13782533791@163.com>
2019-08-11 22:45:47 +0800

    format

--- sort/bubbleSort2.js
+++ sort/bubbleSort2.js
@@ -1,8 +1,8 @@
 /**
- * 
- * @param {number[]} nums 
+ *
+ * @param {number[]} nums
  */
-function bubbleSort(nums) {
+function bubbleSort (nums) {
   for (let i = nums.length - 1; i > 0; i--) {
     for (let j = 0; j < i; j++) {
       if (nums[j] > nums[j + 1]) {
@@ -10,6 +10,6 @@ function bubbleSort(nums) {
       }
     }
   }
-  
+
   return nums
 }
