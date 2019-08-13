eefcfd580ba0bdb132467ce3989577a105fab0c3
qinjunshan <13782533791@163.com>
2019-08-13 09:18:21 +0800

    99:recover-binary-search-tree

--- 99.recover-binary-search-tree.js
+++ 99.recover-binary-search-tree.js
@@ -17,15 +17,40 @@
 var recoverTree = function (root) {
   // 复原二叉搜索树
   // 1.递归，中序遍历
-  let list = []
+  /* let list = []
   let vals = []
   inOrder(root, list, vals)
   vals.sort((a, b) => a - b)
   for (let i = 0; i < vals.length; i++) {
     list[i].val = vals[i]
-  }
+  } */
 
-  // TODO:
+  // 2.递归：利用栈
+  // 三个指针，first，second分别表示第一个和第二个错乱位置的节点，pre指向当前节点p的中序遍历的前一个节点
+  // BST：左 < 中 < 右，即 pre.val < p.val
+  let pre = null
+  let first = null
+  let second = null
+  let p = root
+  let stack = []
+  while (p || stack.length) {
+    while (p) {
+      stack.push(p)
+      p = p.left
+    }
+    p = stack.pop()
+    if (pre) {
+      if (pre.val > p.val) {
+        if (!first) first = pre
+        second = p
+      }
+    }
+    pre = p
+    p = p.right
+  }
+  let tmp = first.val
+  first.val = second.val
+  second.val = tmp
 }
 
 function inOrder (root, list, vals) {
