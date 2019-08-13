25409b7af3eb1e7d9bbe213b57035b490c53f22e
qinjunshan <13782533791@163.com>
2019-08-13 09:38:30 +0800

    643:maximum-avarage-subarray-i

--- 643.maximum-average-subarray-i.js
+++ 643.maximum-average-subarray-i.js
@@ -9,7 +9,8 @@
  * @return {number}
  */
 var findMaxAverage = function (nums, k) {
-  if (nums.length < k) return 0
+  // 1.
+  /* if (nums.length < k) return 0
   let max = Number.MIN_SAFE_INTEGER
   for (let i = 0, len = nums.length; i <= len - k; i++) {
     let arr = nums.slice(i, i + k)
@@ -18,7 +19,19 @@ var findMaxAverage = function (nums, k) {
     })
     max = Math.max(max, tmp / k)
   }
-  return max
+  return max */
+
+  // 2.计算子数组之和的常用方法应该是建立累加数组
+  let len = nums.length
+  let sums = nums.slice()
+  for (let i = 1; i < len; i++) {
+    sums[i] = sums[i - 1] + nums[i]
+  }
+  let max = sums[k - 1]
+  for (let i = k; i < len; i++) {
+    max = Math.max(max, sums[i] - sums[i - k])
+  }
+  return max / k
 }
 
 // findMaxAverage([1, 12, -5, -6, 50, 3], 4)
