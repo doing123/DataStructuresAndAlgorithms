13841ebfb45f81d87fd9ad1922097e1d20aeb38f
qinjunshan <13782533791@163.com>
2019-08-19 09:27:34 +0800

    409:longest-palindrome

---
+++ 409.longest-palindrome.js
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=409 lang=javascript
+ *
+ * [409] Longest Palindrome
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestPalindrome = function (s) {
+  // TODO:
+}
