10a8f334887cd26308e213660a7edd9fb94eb729
qinjunshan <13782533791@163.com>
2019-08-20 09:14:43 +0800

    409:longest-palindrome

--- 409.longest-palindrome.js
+++ 409.longest-palindrome.js
@@ -8,7 +8,8 @@
  * @return {number}
  */
 var longestPalindrome = function (s) {
-  let count = 0
+  // 1.map
+  /* let count = 0
   let map = new Map()
   for (let i = 0; i < s.length; i++) {
     if (map.has(s[i])) {
@@ -30,7 +31,25 @@ var longestPalindrome = function (s) {
     }
   }
 
-  return count + flag
+  return count + flag */
+
+  // 2.a到z是从97到122，A到Z是从65到90，中间相隔58个数
+  let arr = new Array(58).fill(0)
+  for (let i = 0; i < s.length; i++) {
+    arr[s[i].charCodeAt() - 65]++
+  }
+  let count = 0
+  let single = 0
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] % 2 === 0) {
+      count += arr[j]
+    } else {
+      count += arr[j] - 1
+      single = 1
+    }
+  }
+
+  return count + single
 }
 
-// longestPalindrome('abccccdd')
+// longestPalindrome('AAAAAA')
