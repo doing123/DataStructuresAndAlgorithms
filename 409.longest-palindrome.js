50a9c2ac28cc7c72cfe832a1ff95f2d7566e1a5b
qinjunshan <13782533791@163.com>
2019-08-20 08:52:17 +0800

    409:longest-palindrome

--- 409.longest-palindrome.js
+++ 409.longest-palindrome.js
@@ -8,5 +8,30 @@
  * @return {number}
  */
 var longestPalindrome = function (s) {
-  // TODO:
+  let count = 0
+  let map = new Map()
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      map.set(s[i], map.get(s[i]) + 1)
+    } else {
+      map.set(s[i], 1)
+    }
+  }
+  if (map.size === 1) {
+    return s.length
+  }
+  let flag = false
+  for (let val of map.values()) {
+    if (val > 1) {
+      if (val % 2 === 1) flag = true
+      count += Math.floor(val / 2) * 2
+    } else if (!flag) {
+      flag = true
+    }
+  }
+  if (flag) count++
+
+  return count
 }
+
+// longestPalindrome('abccccdd')
