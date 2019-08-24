b7d9a5db32aab50d0c4f9c642e4ead9c2cdeaec5
qinjunshan <13782533791@163.com>
2019-08-24 09:46:24 +0800

    32：longest-valid-parentheses

--- 32.longest-valid-parentheses.js
+++ 32.longest-valid-parentheses.js
@@ -8,5 +8,21 @@
  * @return {number}
  */
 var longestValidParentheses = function (s) {
-  // TODO:
+  let res = 0
+  let start = 0
+  let stack = []
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      stack.push(i)
+    } else if (s[i] === ')') {
+      if (stack.length === 0) {
+        start = i + 1
+      } else {
+        stack.pop()
+        res = stack.length === 0 ? Math.max(res, i - start + 1) : Math.max(res, i - stack[stack.length - 1])
+      }
+    }
+  }
+
+  return res
 }
