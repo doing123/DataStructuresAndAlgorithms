90fc751bbe4842e3ba2ff5670349fb5e0c0f6e70
qinjunshan <13782533791@163.com>
2019-08-27 09:24:33 +0800

    39:combination-sum

--- 39.combination-sum.js
+++ 39.combination-sum.js
@@ -9,5 +9,25 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
-  // TODO:
+  let res = []
+  if (candidates.length === 0) return res
+  candidates.sort((a, b) => a - b)
+  getCombinations(res, [], candidates, target, 0) // 从当前位置向后找
+  return res
 }
+
+function getCombinations (res, arr, candidates, target, pos) {
+  if (target < 0) return
+  if (target === 0) {
+    res.push(arr)
+    return
+  }
+  for (let i = pos; i < candidates.length; i++) {
+    if (candidates[i] > target) break
+    let tmp = arr.slice()
+    tmp.push(candidates[i])
+    getCombinations(res, tmp, candidates, target - candidates[i], i)
+  }
+}
+
+combinationSum([2, 3, 6, 7], 7)
