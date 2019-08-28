80b681fc88b17fcab7d7e02966020e38bf1fd6fb
qinjunshan <13782533791@163.com>
2019-08-28 08:33:22 +0800

    31:next-permutation

--- 31.next-permutation.js
+++ 31.next-permutation.js
@@ -8,5 +8,32 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function (nums) {
-  // TODO:
+  if (nums.length < 2) return nums
+  let partition = -1
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (nums[i] < nums[i + 1]) {
+      partition = i
+      break
+    }
+  }
+  if (partition === -1) {
+    nums.reverse()
+    return nums
+  } else {
+    for (let i = nums.length - 1; i > partition; i--) {
+      if (nums[i] > nums[partition]) {
+        [nums[i], nums[partition]] = [nums[partition], nums[i]]
+        break
+      }
+    }
+  }
+  let left = partition + 1
+  let right = nums.length - 1
+  while (left < right) {
+    [nums[left], nums[right]] = [nums[right], nums[left]]
+    left++
+    right--
+  }
+
+  return nums
 }
