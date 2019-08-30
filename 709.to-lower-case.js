f14768ea3d86a68efc2829ac423f9cf574ca0ae1
qinjunshan <13782533791@163.com>
2019-08-30 08:59:12 +0800

    709：to-lower-case

---
+++ 709.to-lower-case.js
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=709 lang=javascript
+ *
+ * [709] To Lower Case
+ */
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function (str) {
+  return str.toLowerCase()
+}
