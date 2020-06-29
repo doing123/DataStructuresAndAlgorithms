465193b043e5bdeb5a0065e4f5b7f14635d354d5
junshan qin <qinjunshan123@gmail.com>
2020-06-29 21:27:47 +0800

    22.generate-parentheses.js

--- 22.generate-parentheses.js
+++ 22.generate-parentheses.js
@@ -11,21 +11,27 @@
  */
 var generateParenthesis = function (n) {
   const result = [];
-  function generate(left, right, total, s) {
-    if (left === total && right === total) {
+
+  // left：当前已用左括号 right：已用右括号, s：当前字符
+  function generate(left, right, s) {
+    //  终止条件：如果左右括弧都用完则结束
+    if (s.length === 2 * n) {
       result.push(s);
       return;
     }
 
+    // 如果左括弧未用完则继续增加左括弧
     if (left < n) {
-      generate(left + 1, right, total, s + '(');
+      generate(left + 1, right, s + '(');
     }
+
+    // 如果右括弧少于左括弧则继续增加右括弧
     if (left > right) {
-      generate(left, right + 1, total, s + ')');
+      generate(left, right + 1, s + ')');
     }
   }
 
-  generate(0, 0, n, '');
+  generate(0, 0, '');
 
   return result;
 };
