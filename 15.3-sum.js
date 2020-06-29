cfb83bb9e02434a19e65cdb39555207ab5e779d0
junshan qin <qinjunshan123@gmail.com>
2020-06-29 20:53:01 +0800

    15.3-sum.js

--- 15.3-sum.js
+++ 15.3-sum.js
@@ -8,31 +8,31 @@
  * @return {number[][]}
  */
 var threeSum = function (nums) {
-  let res = []
-  if (nums.length < 3) return res
-  nums.sort((a, b) => a - b)
-
+  const result = [];
+  if (nums.length < 3) return result;
+  nums.sort((a, b) => a - b);
   for (let i = 0; i < nums.length - 2; i++) {
-    if (nums[i] > 0) break
-    if (i > 0 && nums[i] === nums[i - 1]) continue
-    for (let j = i + 1, k = nums.length - 1; j < k;) {
-      if (nums[i] + nums[j] + nums[k] === 0) {
-        res.push([nums[i], nums[j], nums[k]])
-        j++
-        k--
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1, k = nums.length - 1; j < k; ) {
+      const sum = nums[i] + nums[j] + nums[k];
+      if (sum === 0) {
+        result.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
         while (j < k && nums[j] === nums[j - 1]) {
-          j++
+          j++;
         }
         while (j < k && nums[k] === nums[k + 1]) {
-          k--
+          k--;
         }
-      } else if (nums[i] + nums[j] + nums[k] > 0) {
-        k--
+      } else if (sum > 0) {
+        k--;
       } else {
-        j++
+        j++;
       }
     }
   }
 
-  return res
-}
+  return result;
+};
