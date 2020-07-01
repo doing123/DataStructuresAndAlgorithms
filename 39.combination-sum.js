855db13f98ee6cfa77985cad3d735be91832afb3
junshan qin <qinjunshan123@gmail.com>
2020-07-01 16:55:54 +0800

    39.combination-sum.js

--- 39.combination-sum.js
+++ 39.combination-sum.js
@@ -9,25 +9,26 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
-  let res = []
-  if (candidates.length === 0) return res
-  candidates.sort((a, b) => a - b)
-  getCombinations(res, [], candidates, target, 0) // 从当前位置向后找
-  return res
-}
+  let res = [];
+  if (candidates.length === 0) return res;
+  candidates.sort((a, b) => a - b);
+  getCombinations(res, [], candidates, target, 0); // 从当前位置向后找
+  return res;
+};
 
-function getCombinations (res, arr, candidates, target, pos) {
-  if (target < 0) return
+function getCombinations(res, arr, candidates, target, pos) {
+  if (target < 0) return;
   if (target === 0) {
-    res.push(arr)
-    return
+    res.push(arr);
+    return;
   }
+
   for (let i = pos; i < candidates.length; i++) {
-    if (candidates[i] > target) break
-    let tmp = arr.slice()
-    tmp.push(candidates[i])
-    getCombinations(res, tmp, candidates, target - candidates[i], i)
+    if (candidates[i] > target) break;
+    let tmp = arr.slice();
+    tmp.push(candidates[i]);
+    getCombinations(res, tmp, candidates, target - candidates[i], i);
   }
 }
 
-combinationSum([2, 3, 6, 7], 7)
+// combinationSum([2, 3, 6, 7], 7)
