8a778327cdd0735a22594dee6e2af2214ca5e806
junshan qin <qinjunshan123@gmail.com>
2020-07-07 22:21:11 +0800

    74.搜索二维矩阵.js 二分查找

--- 74.搜索二维矩阵.js
+++ 74.搜索二维矩阵.js
@@ -10,11 +10,24 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchMatrix = function(matrix, target) {
-  for (let i = 0; i < matrix.length; i++) {
-    const len = matrix[i].length;
-    if (target <= matrix[i][len -1] && matrix[i].includes(target)) {
-      return true;
+var searchMatrix = function (matrix, target) {
+  let row = matrix.length;
+  if (row == 0) return false;
+  let col = matrix[0].length;
+  let left = 0;
+  let right = row * col - 1;
+
+  // 优化：二分查找
+  let midIndex;
+  let midItem;
+  while (left <= right) {
+    midIndex = Math.floor((right + left) / 2);
+    midItem = matrix[Math.floor(midIndex / col)][midIndex % col];
+    if (midItem == target) return true;
+    if (midItem > target) {
+      right = midIndex - 1;
+    } else {
+      left = midIndex + 1;
     }
   }
   return false;
