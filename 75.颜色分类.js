13d812c8048a99c6390e24c13b1ad18cb44ad09b
junshan qin <qinjunshan123@gmail.com>
2020-07-08 19:44:52 +0800

    75.颜色分类.js

--- 75.颜色分类.js
+++ 75.颜色分类.js
@@ -10,18 +10,40 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var sortColors = function (nums) {
-  let len = nums.length;
-  let count = 0;
-  for (let i = 0; i < len; i++) {
-    if (nums[i] == 1) {
+  // let len = nums.length;
+  // let count = 0;
+  // for (let i = 0; i < len; i++) {
+  //   if (nums[i] == 1) {
+  //     continue;
+  //   } else if (nums[i] == 0) {
+  //     nums.unshift(...nums.splice(i, 1));
+  //   } else if (nums[i] == 2) {
+  //     nums.push(...nums.splice(i, 1));
+  //     i--;
+  //   }
+  //   if (++count >= len) return;
+  // }
+
+  // 三指针
+  let left = 0; // 0 的最右边界
+  let right = nums.length - 1; // 2 的最左边界
+  let i = 0; // 当前
+  while (i <= right) {
+    if (nums[i] == 0) {
+      [nums[left], nums[i]] = [nums[i], nums[left]];
+      left++;
+      i++;
       continue;
-    } else if (nums[i] == 0) {
-      nums.unshift(...nums.splice(i, 1));
-    } else if (nums[i] == 2) {
-      nums.push(...nums.splice(i, 1));
-      i--;
     }
-    if (++count >= len) return;
+    if (nums[i] == 2) {
+      [nums[right], nums[i]] = [nums[i], nums[right]];
+      right--;
+      continue;
+    }
+    if (nums[i] == 1) {
+      i++;
+      continue;
+    }
   }
 };
 // @lc code=end
