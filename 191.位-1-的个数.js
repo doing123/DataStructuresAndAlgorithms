7047f6712c6ca1d879108ce5e25cd115ce55305f
junshan qin <qinjunshan123@gmail.com>
2020-07-13 21:32:40 +0800

    191.位-1-的个数.js

--- 191.位-1-的个数.js
+++ 191.位-1-的个数.js
@@ -11,13 +11,21 @@
  */
 var hammingWeight = function (n) {
   // 解法一：循环和位移动
+  // let count = 0;
+  // let mask = 1;
+  // for (let i = 0; i < 32; i++) {
+  //   if ((mask & n) != 0) {
+  //     count++;
+  //   }
+  //   mask <<= 1;
+  // }
+  // return count;
+
+  // 解法二：位操作技巧
   let count = 0;
-  let mask = 1;
-  for (let i = 0; i < 32; i++) {
-    if ((mask & n) != 0) {
-      count++;
-    }
-    mask <<= 1;
+  while (n != 0) {
+    count++;
+    n &= n - 1;
   }
   return count;
 };
