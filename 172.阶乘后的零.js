6d0f9ed17659f175a182decc7987d5f4fc2bebbd
junshan qin <qinjunshan123@gmail.com>
2020-07-13 21:01:43 +0800

    172.阶乘后的零.js

---
+++ 172.阶乘后的零.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=172 lang=javascript
+ *
+ * [172] 阶乘后的零
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function (n) {
+  var count = 0;
+  while (n >= 5) {
+    count += (n / 5) | 0; // TODO 没理解
+    n /= 5;
+  }
+  return count;
+};
+// @lc code=end
