96eedabb684f8dd517bad95f942ae3f63a3c0e9f
junshan qin <qinjunshan123@gmail.com>
2020-07-16 21:22:45 +0800

    228.汇总区间.js

--- 228.汇总区间.js
+++ 228.汇总区间.js
@@ -10,33 +10,34 @@
  * @return {string[]}
  */
 var summaryRanges = function (nums) {
+  // 优化
+  let result = [];
+  let start = 0;
+  for (let i = 0, len = nums.length; i < len; i++) {
+    if (i + 1 < len && nums[i + 1] - nums[i] === 1) continue;
+    if (start == i) {
+      result.push(`${nums[i]}`);
+    } else {
+      result.push(`${nums[start]}->${nums[i]}`);
+    }
+    start = i + 1;
+  }
+  return result;
+
+  // 2.哨兵
   // let result = [];
   // let start = 0;
-  // for (let i = 0, len = nums.length; i < len; i++) {
-  //   if (nums[i + 1] - nums[i] > 1 || i == len - 1) {
-  //     if (i - start > 0) {
-  //       result.push(`${nums[start]}->${nums[i]}`);
+  // nums.push(null); // 哨兵
+  // for (let i = 1, len = nums.length; i < len; i++) {
+  //   if (nums[i] - nums[i - 1] > 1 || nums[i] == null) {
+  //     if (i - start > 1) {
+  //       result.push(`${nums[start]}->${nums[i - 1]}`);
   //     } else {
-  //       result.push(`${nums[i]}`);
+  //       result.push(`${nums[i - 1]}`);
   //     }
-  //     start = i + 1;
+  //     start = i;
   //   }
   // }
   // return result;
-
-  let result = [];
-  let start = 0;
-  nums.push(null); // 哨兵
-  for (let i = 1, len = nums.length; i < len; i++) {
-    if (nums[i] - nums[i - 1] > 1 || nums[i] == null) {
-      if (i - start > 1) {
-        result.push(`${nums[start]}->${nums[i - 1]}`);
-      } else {
-        result.push(`${nums[i - 1]}`);
-      }
-      start = i;
-    }
-  }
-  return result;
 };
 // @lc code=end
