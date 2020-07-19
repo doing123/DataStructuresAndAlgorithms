fa205732947c578bf62b853616741c02d158cee6
junshan qin <qinjunshan123@gmail.com>
2020-07-19 16:07:29 +0800

    162.寻找峰值.js(二分查找：迭代)

--- 162.寻找峰值.js
+++ 162.寻找峰值.js
@@ -10,12 +10,17 @@
  * @return {number}
  */
 var findPeakElement = function (nums) {
-  // 1.线性扫描
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > nums[i + 1]) {
-      return i;
+  // 2.二分查找：迭代
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    let mid = left + ((right - left) >> 2);
+    if (nums[mid] > nums[mid + 1]) {
+      right = mid;
+    } else {
+      left = mid + 1;
     }
   }
-  return nums.length - 1;
+  return left;
 };
 // @lc code=end
