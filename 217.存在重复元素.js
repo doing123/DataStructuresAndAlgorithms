998a28d8a457b5bac1f952f6a9ac509b441be392
junshan qin <qinjunshan123@gmail.com>
2020-07-20 21:15:19 +0800

    217.存在重复元素.js

---
+++ 217.存在重复元素.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=javascript
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  // 1.暴力 双循环
+  for (let i = 0, len = nums.length; i < len; i++) {
+    for (let j = i + 1; j < len; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+// @lc code=end
