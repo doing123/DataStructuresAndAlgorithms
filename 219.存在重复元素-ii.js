65d6379c3ed3bed789af8228deead03b9a1cf13f
junshan qin <qinjunshan123@gmail.com>
2020-07-20 21:03:44 +0800

    219.存在重复元素-ii.js

---
+++ 219.存在重复元素-ii.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=javascript
+ *
+ * [219] 存在重复元素 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function (nums, k) {
+  for (let i = 0; i < nums.length; i++) {
+    let target = nums[i];
+    let index = nums.slice(i + 1, i + k + 1).indexOf(target);
+    if (index > -1 && index - i <= k) {
+      return true;
+    }
+  }
+  return false;
+};
+// @lc code=end
