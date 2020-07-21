fd224dc67487caea391861a014df4ea135d6fbaa
junshan qin <qinjunshan123@gmail.com>
2020-07-21 21:36:59 +0800

    374.猜数字大小.js

---
+++ 374.猜数字大小.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=374 lang=javascript
+ *
+ * [374] 猜数字大小
+ */
+
+// @lc code=start
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let left = 1;
+  let right = n;
+  let mid;
+  while (left <= right) {
+    mid = parseInt((left + right) / 2);
+    let val = guess(mid);
+    if (val == -1) {
+      right = mid - 1;
+    } else if (val == 1) {
+      left = mid + 1;
+    } else if (val == 0) {
+      return mid;
+    }
+  }
+};
+// @lc code=end
