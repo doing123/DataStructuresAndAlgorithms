a7acbf917ec2434e11f4cdb17b296fc7b7cdac6a
junshan qin <qinjunshan123@gmail.com>
2020-07-21 20:42:40 +0800

    287.寻找重复数.js

--- 287.寻找重复数.js
+++ 287.寻找重复数.js
@@ -10,13 +10,23 @@
  * @return {number}
  */
 var findDuplicate = function (nums) {
-  let map = {};
-  for (let i = 0; i < nums.length; i++) {
-    let curr = nums[i];
-    if (map[curr]) {
-      return curr;
-    }
-    map[curr] = true;
+  // TODO 1.二分查找
+  // 2.快慢指针
+  let slow = 0;
+  let fast = 0;
+  do {
+    slow = nums[slow]; // 慢指针走一步，快指针走两步
+    fast = nums[nums[fast]];
+  } while (slow != fast);
+
+  slow = 0;
+  while (slow != fast) {
+    slow = nums[slow];
+    fast = nums[fast];
   }
+
+  return slow;
+
+  // TODO 3.位运算
 };
 // @lc code=end
