5a06f524954219304cbd5377847522b8b02528c2
junshan qin <qinjunshan123@gmail.com>
2020-07-22 21:30:34 +0800

    414.第三大的数.js

--- 414.第三大的数.js
+++ 414.第三大的数.js
@@ -10,7 +10,24 @@
  * @return {number}
  */
 var thirdMax = function (nums) {
-  let arr = [...new Set(nums)].sort((a, b) => b - a);
-  return arr.length >= 3 ? arr[2] : arr[0];
+  let one = nums[0]; // 第一大
+  let two = -Infinity; // 第二大
+  let three = -Infinity; // 第三大
+  for (let i = 0; i < nums.length; i++) {
+    let item = nums[i];
+    if (item == one || item == two || item == three) continue;
+    if (item > one) {
+      three = two;
+      two = one;
+      one = item;
+    } else if (item > two) {
+      three = two;
+      two = item;
+    } else if (item > three) {
+      three = item;
+    }
+  }
+
+  return nums.length >= 3 && three != -Infinity ? three : one;
 };
 // @lc code=end
