78dec20f472bd5a457716a3594a0b9250599565c
junshan qin <qinjunshan123@gmail.com>
2020-07-22 21:18:53 +0800

    414.第三大的数.js

---
+++ 414.第三大的数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=414 lang=javascript
+ *
+ * [414] 第三大的数
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function (nums) {
+  let arr = [...new Set(nums)].sort((a, b) => b - a);
+  return arr.length >= 3 ? arr[2] : arr[0];
+};
+// @lc code=end
