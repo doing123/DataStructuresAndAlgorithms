8692d767a3a8bdaaa241851703b73b03e93e92aa
junshan qin <qinjunshan123@gmail.com>
2020-07-23 20:30:01 +0800

    407.接雨水-ii.js

---
+++ 407.接雨水-ii.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=407 lang=javascript
+ *
+ * [407] 接雨水 II
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} heightMap
+ * @return {number}
+ */
+var trapRainWater = function(heightMap) {
+  // TODO hard 先放弃
+};
+// @lc code=end
