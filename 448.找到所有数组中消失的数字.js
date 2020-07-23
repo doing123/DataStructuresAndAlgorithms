f0d062c4861e3ab62463ebceb4022ed74da9194c
junshan qin <qinjunshan123@gmail.com>
2020-07-23 21:41:42 +0800

    448.找到所有数组中消失的数字.js

---
+++ 448.找到所有数组中消失的数字.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=448 lang=javascript
+ *
+ * [448] 找到所有数组中消失的数字
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findDisappearedNumbers = function (nums) {
+  let result = [];
+  // 利用索引，改为负数，如 num, 对应索引 num - 1，如为 正数 取反，最后数组中为正的值的 “索引 +1” 即为消失的数字
+  for (let i = 0; i < nums.length; i++) {
+    let index = Math.abs(nums[i]) - 1;
+    if (nums[index] > 0) {
+      nums[index] = -nums[index];
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > 0) {
+      result.push(i + 1);
+    }
+  }
+
+  return result;
+};
+// @lc code=end
