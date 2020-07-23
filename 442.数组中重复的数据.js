1901b88c1b5be57ce5682b050914d1022670bf95
junshan qin <qinjunshan123@gmail.com>
2020-07-23 21:22:28 +0800

    442.数组中重复的数据.js

--- 442.数组中重复的数据.js
+++ 442.数组中重复的数据.js
@@ -11,22 +11,14 @@
  */
 var findDuplicates = function (nums) {
   let result = [];
-  // 通过索引号排序, 数字4放到索引3的位置,
+  // 利用索引号,如数字num，将num - 1的位置的数字取相反数，连续两次取相反数会变回来
   for (let i = 0; i < nums.length; i++) {
-    // 当前项 item - 1，所对应的位置 不为 item，则互换
-    while (nums[nums[i] - 1] != nums[i]) {
-      // [nums[i], nums[nums[i] - 1]] = [nums[nums[i] - 1], nums[i]]; // TODO 要记录位置，这里互换跑不通
-      var tmp = nums[i];
-      let index = nums[i] - 1; // 要保持这个位置
-      nums[i] = nums[index];
-      nums[index] = tmp;
+    let index = Math.abs(nums[i]) - 1;
+    // 如果为负，即已经出现过一次了
+    if (nums[index] < 0) {
+      result.push(index + 1);
     }
-  }
-  for (let i = 0; i < nums.length; i++) {
-    // 找排序后数组，与索引号没有相差1便是重复元素
-    if (nums[i] != i + 1) {
-      result.push(nums[i]);
-    }
+    nums[index] = -nums[index];
   }
 
   return result;
