8b1da25d66e8e9133f190205bfb126217b5fe406
junshan qin <qinjunshan123@gmail.com>
2020-07-25 11:06:47 +0800

    457.环形数组循环.js

--- 457.环形数组循环.js
+++ 457.环形数组循环.js
@@ -10,7 +10,7 @@
  * @return {boolean}
  */
 var circularArrayLoop = function (nums) {
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0, len = nums.length; i < len; i++) {
     if (nums[i] === 0) continue;
     let set = new Set();
     let item = nums[i];
@@ -20,7 +20,7 @@ var circularArrayLoop = function (nums) {
       if (flag != item > 0) break;
       if (set.has(index) && set.size > 1) return true;
       set.add(index);
-      let next = (index + nums.length * 1000 + nums[index]) % nums.length; // 加上 nums.length * 1000，考虑负的索引
+      let next = (((nums[index] + index) % len) + len) % len; // 加上 nums.length * 1000，考虑负的索引
       if (next === index) break;
       index = next;
       item = nums[index];
