b381edc9c715cd71b6bec7898b89b79dec162b00
junshan qin <qinjunshan123@gmail.com>
2020-07-31 16:35:57 +0800

    689.三个无重叠子数组的最大和.js

---
+++ 689.三个无重叠子数组的最大和.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=689 lang=javascript
+ *
+ * [689] 三个无重叠子数组的最大和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSumOfThreeSubarrays = function(nums, k) {
+  // TODO
+};
+// @lc code=end
