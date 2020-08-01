0867f3a0404be5a88f0d576eeb006c98e453dc27
junshan qin <qinjunshan123@gmail.com>
2020-08-01 15:19:43 +0800

    717.1-比特与-2-比特字符.js

--- 717.1-比特与-2-比特字符.js
+++ 717.1-比特与-2-比特字符.js
@@ -10,15 +10,7 @@
  * @return {boolean}
  */
 var isOneBitCharacter = function (bits) {
-  // 当成个队列, 碰到 0, 就出列一个, 碰到1就出列两个
-  while (bits.length > 1) {
-    if (bits[0] === 1) {
-      bits.shift();
-      bits.shift();
-    } else {
-      bits.shift();
-    }
-  }
-  return bits.length === 1;
+  // 正则表达式
+  return /^(10||11||0)*0$/.test(bits.join(''));
 };
 // @lc code=end
