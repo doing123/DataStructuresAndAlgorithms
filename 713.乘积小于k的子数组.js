35033c00e6947d779bf171c11c840bcdd731fbb2
junshan qin <qinjunshan123@gmail.com>
2020-08-01 14:42:42 +0800

    713.乘积小于k的子数组.js

--- 713.乘积小于k的子数组.js
+++ 713.乘积小于k的子数组.js
@@ -23,6 +23,7 @@ var numSubarrayProductLessThanK = function (nums, k) {
       total = total / nums[left];
       left++;
     }
+    // 滑动窗口：原来数组中有几个数，新增加一个数，就会增加几个连续子数组
     result += right - left + 1; // 以右指针为首增加子数组，[5,2]加6，增加[6]、[2,6]、[5,2,6]，个数即为R - L+ 1
     right++;
   }
