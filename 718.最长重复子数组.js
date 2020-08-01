c00fda29d048cf623d66ce267982d4ff35adf917
junshan qin <qinjunshan123@gmail.com>
2020-08-01 16:16:09 +0800

    718.最长重复子数组.js

--- 718.最长重复子数组.js
+++ 718.最长重复子数组.js
@@ -11,21 +11,21 @@
  * @return {number}
  */
 var findLength = function (A, B) {
-  // 53/54 cases passed
+  // 动态规划
   let result = 0;
-  for (let i = 0; i < A.length; i++) {
-    let index = B.indexOf(A[i]);
-    while (index > -1) {
-      let count = 0;
-      let j = i;
-      let k = index;
-      while (A[j] === B[k] && j < A.length && k < B.length) {
-        count++;
-        j++;
-        k++;
+  let aLen = A.length;
+  let bLen = B.length;
+  let dp = Array.from(new Array(aLen + 1), () => {
+    return new Array(bLen + 1).fill(0); // A、B任意长度为0，即没有公共部分，dp[i][j] = 0
+  });
+
+  for (let i = 1; i <= aLen; i++) {
+    for (let j = 1; j <= bLen; j++) {
+      if (A[i - 1] === B[j - 1]) {
+        // 当出现相同元素时，公共前缀等于前缀数组的【末尾项】的公共前缀 + 1
+        dp[i][j] = dp[i - 1][j - 1] + 1;
       }
-      result = Math.max(count, result);
-      index = B.indexOf(A[i], index + 1); // B中可能有多个元素等于A[i]的值
+      result = Math.max(result, dp[i][j]);
     }
   }
 
