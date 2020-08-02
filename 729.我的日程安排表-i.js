6d855a2e7ee6c2246b91f5d83daaddf82e636d7e
junshan qin <qinjunshan123@gmail.com>
2020-08-02 11:21:52 +0800

    729.我的日程安排表-i.js

---
+++ 729.我的日程安排表-i.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=729 lang=javascript
+ *
+ * [729] 我的日程安排表 I
+ */
+
+// @lc code=start
+
+var MyCalendar = function () {
+  this.map = new Map();
+};
+
+/**
+ * @param {number} start
+ * @param {number} end
+ * @return {boolean}
+ */
+MyCalendar.prototype.book = function (start, end) {
+  for (let [key, value] of this.map) {
+    if (key[0] < end && start < key[1]) {
+      return false;
+    }
+  }
+  this.map.set([start, end], true);
+  return true;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * var obj = new MyCalendar()
+ * var param_1 = obj.book(start,end)
+ */
+// @lc code=end
