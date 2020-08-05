3c8cffd413cb504f5c82751fa5cc69e16bb0f8dc
junshan qin <qinjunshan123@gmail.com>
2020-08-05 22:56:58 +0800

    234.回文链表.js

--- 234.回文链表.js
+++ 234.回文链表.js
@@ -16,15 +16,25 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-  let left = '';
-  let right = '';
-  let node = head;
-  while(node) {
-    left += node.val;
-    right = node.val + right;
-    node = node.next;
+var isPalindrome = function (head) {
+  // 快慢指针
+  if (!head || !head.next) return true;
+  let prev = null;
+  let slow = head;
+  let reserved = null; // 保留前半部分链表，且已反转
+  while (head && head.next) {
+    prev = slow;
+    slow = slow.next;
+    head = head.next.next;
+    prev.next = reserved;
+    reserved = prev;
   }
-  return left === right;
+  if (head) slow = slow.next; // 长度为奇数
+  while (slow) {
+    if (slow.val !== reserved.val) return false;
+    slow = slow.next;
+    reserved = reserved.next;
+  }
+  return true;
 };
 // @lc code=end
