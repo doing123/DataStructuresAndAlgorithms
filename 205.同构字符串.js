201c957aad4e009711048b4a81d9a68e9b5ee380
junshan qin <qinjunshan123@gmail.com>
2020-08-06 22:05:06 +0800

    205.同构字符串.js

--- 205.同构字符串.js
+++ 205.同构字符串.js
@@ -11,9 +11,13 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
-  // indexOf 得到对应字母第一次出现的下标 判断两者是否相等
+  // 2.映射
+  let mapS = {};
+  let mapT = {};
   for (let i = 0; i < s.length; i++) {
-    if (s.indexOf(s[i]) != t.indexOf(t[i])) return false;
+    if (!mapS[s[i]]) mapS[s[i]] = t[i];
+    if (!mapT[t[i]]) mapT[t[i]] = s[i];
+    if (mapS[s[i]] != t[i] || mapT[t[i]] != s[i]) return false;
   }
   return true;
 };
