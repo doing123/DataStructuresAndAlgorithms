81aa3d23cfc24f8068e68ff5da769c8f28f86ced
junshan qin <qinjunshan123@gmail.com>
2020-08-11 23:14:18 +0800

    326.3-的幂.js

--- 326.3-的幂.js
+++ 326.3-的幂.js
@@ -10,10 +10,16 @@
  * @return {boolean}
  */
 var isPowerOfThree = function (n) {
-  // 2.递归
-  if (n < 3) {
-    return n == 1;
-  }
-  return isPowerOfThree(n / 3);
+  // 3.转为 3 进制
+  /**
+   * 1 1
+   * 2 2
+   * 3 10
+   * 4 11
+   * 6 20
+   * 9 100
+   * 27 1000
+   */
+  return /^10*$/.test(n.toString(3));
 };
 // @lc code=end
