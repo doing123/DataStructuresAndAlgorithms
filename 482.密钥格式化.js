a2751d1b3f6e176519be7ce6a0e52ac219f122fb
junshan qin <qinjunshan123@gmail.com>
2020-08-17 22:25:58 +0800

    482.密钥格式化.js

---
+++ 482.密钥格式化.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=482 lang=javascript
+ *
+ * [482] 密钥格式化
+ */
+
+// @lc code=start
+/**
+ * @param {string} S
+ * @param {number} K
+ * @return {string}
+ */
+var licenseKeyFormatting = function (S, K) {
+  // 从后遍历
+  S = S.replace(/-/g, '');
+  let result = '';
+  let i = S.length - K;
+  while (i + K > 0) {
+    result = (i > 0 ? '-' : '') + S.substring(i, i + K) + result;
+    i -= K;
+  }
+  return result.toUpperCase();
+};
+// @lc code=end
