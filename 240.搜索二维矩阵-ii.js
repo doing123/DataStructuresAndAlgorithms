4832bd1293f96e821fc301cd5f64e8b0dcf0fee2
junshan qin <qinjunshan123@gmail.com>
2020-08-22 12:43:15 +0800

    240.搜索二维矩阵-ii.js

--- 240.搜索二维矩阵-ii.js
+++ 240.搜索二维矩阵-ii.js
@@ -11,8 +11,21 @@
  * @return {boolean}
  */
 var searchMatrix = function (matrix, target) {
-  // 1.二维数组转为一维数组 超时
-  let arr = matrix.flat();
-  return arr.indexOf(target) > -1 ? true : false;
+  // 2.二分查找
+  for (item of matrix) {
+    if (item[0] > target) return false;
+    let left = 0;
+    let right = item.length - 1;
+    while (left <= right) {
+      let mid = (left + right) >>> 1;
+      if (item[mid] === target) return true;
+      if (item[mid] > target) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    }
+  }
+  return false;
 };
 // @lc code=end
