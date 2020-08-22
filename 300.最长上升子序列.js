18d792eecf0eefa59e6aa77be83a5d20e40c7ad9
junshan qin <qinjunshan123@gmail.com>
2020-08-22 15:58:08 +0800

    300.最长上升子序列.js

---
+++ 300.最长上升子序列.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长上升子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  // 1.动态规划
+  let len = nums.length;
+  if (len == 0) return 0;
+  let dp = new Array(len).fill(1);
+  for (let i = 1; i < len; i++) {
+    // 状态转移方程：
+    // 定义状态要有两个意识：1.最后一步是什么；2.化成子问题；
+    // 以nums[i]结尾，不断的去找前面比自己小的
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp);
+};
+// @lc code=end
