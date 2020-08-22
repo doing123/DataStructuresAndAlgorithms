b676355568e23d1ea698cf7d688f1f16b87a3bcd
junshan qin <qinjunshan123@gmail.com>
2020-08-22 16:47:08 +0800

    315.计算右侧小于当前元素的个数.js

--- 315.计算右侧小于当前元素的个数.js
+++ 315.计算右侧小于当前元素的个数.js
@@ -10,21 +10,8 @@
  * @return {number[]}
  */
 var countSmaller = function (nums) {
-  // 1.暴力
-  let len = nums.length;
-  if (len == 0) return [];
-  let counts = [];
-  for (let i = 0; i < len; i++) {
-    let j = i + 1;
-    let count = 0;
-    while (j < len) {
-      if (nums[j] < nums[i]) {
-        count++;
-      }
-      j++;
-    }
-    counts.push(count);
-  }
-  return counts;
+  // TODO
+  // 2.离散化树状数组，
+  // 3.归并排序
 };
 // @lc code=end
