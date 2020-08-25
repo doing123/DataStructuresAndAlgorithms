8ecdd0fa337496514f0cdd5391dd7c5fc6bb39c6
junshan qin <qinjunshan123@gmail.com>
2020-08-25 23:03:00 +0800

    235.二叉搜索树的最近公共祖先.js

---
+++ 235.二叉搜索树的最近公共祖先.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=javascript
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  // 1.迭代
+  while (true) {
+    if (root.val > Math.max(p.val, q.val)) {
+      root = root.left;
+    } else if (root.val < Math.min(p.val, q.val)) {
+      root = root.right;
+    } else {
+      break;
+    }
+  }
+  return root;
+};
+// @lc code=end
