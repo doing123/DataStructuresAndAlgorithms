04c2fce02ed60ef490be04b1a9360a0e173676cf
junshan qin <qinjunshan123@gmail.com>
2020-08-31 23:42:57 +0800

    142.环形链表-ii.js

--- 142.环形链表-ii.js
+++ 142.环形链表-ii.js
@@ -21,7 +21,7 @@ var detectCycle = function (head) {
   if (!head) return null;
   let slow = head;
   let fast = head;
-  while (fast.next && fast.next.next) {
+  while (fast && fast.next) {
     fast = fast.next.next;
     slow = slow.next;
     if (fast === slow) {
