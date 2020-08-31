397716d778b7bf76775b06a84fcc118d63624867
junshan qin <qinjunshan123@gmail.com>
2020-08-31 23:35:35 +0800

    142.环形链表-ii.js

---
+++ 142.环形链表-ii.js
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=javascript
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  if (!head) return null;
+  let slow = head;
+  let fast = head;
+  while (fast.next && fast.next.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+    if (fast === slow) {
+      slow = head;
+      while (slow !== fast) {
+        slow = slow.next;
+        fast = fast.next;
+      }
+      return slow;
+    }
+  }
+  return null;
+};
+// @lc code=end
