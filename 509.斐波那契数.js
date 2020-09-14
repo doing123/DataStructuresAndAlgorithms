fa9e50cc49582dd0b1305522a1ae8ed5aa1c70de
junshan qin <qinjunshan123@gmail.com>
2020-09-14 22:32:32 +0800

    509.斐波那契数.js

---
+++ 509.斐波那契数.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function (N) {
+  if (N === 0) return 0;
+  if (N === 1) return 1;
+  return fib(N - 1) + fib(N - 2);
+};
+// @lc code=end
