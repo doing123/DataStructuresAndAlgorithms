6d7d64bccf2b69c8867975f8a1ee0f327a32320f
junshan qin <qinjunshan123@gmail.com>
2020-09-15 23:11:46 +0800

    242.有效的字母异位词.js

--- 242.有效的字母异位词.js
+++ 242.有效的字母异位词.js
@@ -11,7 +11,20 @@
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  // 2.转数组
-  return s.split('').sort().join() === t.split('').sort().join()
+  // 3.数组
+  if (s === t) return true;
+  if (s.length !== t.length) return false;
+  let sArr = Array.from(new Array(26)).fill(0);
+  // let tArr = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    let index = s[i].charCodeAt() - 97;
+    sArr[index] += 1;
+    index = t[i].charCodeAt() - 97;
+    sArr[index] -= 1;
+  }
+  for (item of sArr) {
+    if (item !== 0) return false;
+  }
+  return true;
 };
 // @lc code=end
