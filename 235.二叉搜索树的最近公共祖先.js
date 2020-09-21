900a5b1446b0a5491b8836f294672e30a14b27cd
junshan qin <qinjunshan123@gmail.com>
2020-09-21 22:28:03 +0800

    235.二叉搜索树的最近公共祖先.js

--- 235.二叉搜索树的最近公共祖先.js
+++ 235.二叉搜索树的最近公共祖先.js
@@ -20,13 +20,15 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-  // 2.递归
-  if (root.val > Math.max(p.val, q.val)) {
-    return lowestCommonAncestor(root.left, p, q);
-  } else if (root.val < Math.min(p.val, q.val)) {
-    return lowestCommonAncestor(root.right, p, q);
-  } else {
-    return root;
+  // 1.迭代
+  while (root) {
+    if (root.val > Math.max(p.val, q.val)) {
+      root = root.left
+    } else if (root.val < Math.min(p.val, q.val)) {
+      root = root.right
+    } else {
+      return root
+    }
   }
-};
+}
 // @lc code=end
