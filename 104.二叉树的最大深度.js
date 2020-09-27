e877ec111ff7663185f92384eef411ec19fcda0c
junshan qin <qinjunshan123@gmail.com>
2020-09-27 22:47:34 +0800

    104.二叉树的最大深度.js

--- 104.二叉树的最大深度.js
+++ 104.二叉树的最大深度.js
@@ -17,15 +17,15 @@
  * @return {number}
  */
 var maxDepth = function (root) {
-  // 1.BFS
+  // 1.BFS 2
   if (!root) return 0;
   let level = 0;
   let queue = [root];
   while (queue.length) {
     level++;
-    let len = queue.length;
-    for (let i = 0; i < len; i++) {
-      let node = queue.shift();
+    let arr = [...queue];
+    queue = [];
+    for (let node of arr) {
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
     }
