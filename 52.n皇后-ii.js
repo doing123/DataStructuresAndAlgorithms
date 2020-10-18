ce8f20001ecf357dd05211d12676e607456d26b0
junshan qin <qinjunshan123@gmail.com>
2020-10-18 10:36:55 +0800

    52.n皇后-ii.js

---
+++ 52.n皇后-ii.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=52 lang=javascript
+ *
+ * [52] N皇后 II
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var totalNQueens = function (n) {
+  let result = 0;
+  let cols = new Set();
+  let pie = new Set();
+  let na = new Set();
+
+  let helper = (row) => {
+    if (row >= n) result++;
+    for (let col = 0; col < n; col++) {
+      if (cols.has(col) || pie.has(row + col) || na.has(row - col)) continue;
+      cols.add(col);
+      pie.add(row + col);
+      na.add(row - col);
+
+      helper(row + 1); // 下一行
+
+      cols.delete(col);
+      pie.delete(row + col);
+      na.delete(row - col);
+    }
+  };
+
+  helper(0);
+  return result;
+};
+// @lc code=end
