52a019789ee21333fdf6c54defd5db4021f4a84d
junshan qin <qinjunshan123@gmail.com>
2020-10-19 22:21:38 +0800

    42.trapping-rain-water.js

--- 42.trapping-rain-water.js
+++ 42.trapping-rain-water.js
@@ -8,5 +8,30 @@
  * @return {number}
  */
 var trap = function (height) {
-  // TODO:
-}
+  if (height.length < 3) return 0;
+  let left = 0;
+  let right = height.length - 1;
+  let leftMax = 0;
+  let rightMax = 0;
+  let result = 0;
+
+  while (left < right) {
+    if (height[left] < height[right]) {
+      if (height[left] > leftMax) {
+        leftMax = height[left];
+      } else {
+        result += leftMax - height[left];
+      }
+      left++;
+    } else {
+      if (height[right] > rightMax) {
+        rightMax = height[right];
+      } else {
+        result += rightMax - height[right];
+      }
+      right--;
+    }
+  }
+
+  return result;
+};
