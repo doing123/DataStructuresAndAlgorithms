041f01304657b6f9bf2f9998ad47d7e3f320596c
junshan qin <qinjunshan123@gmail.com>
2020-10-24 17:38:54 +0800

    231.2-的幂.js

--- 231.2-的幂.js
+++ 231.2-的幂.js
@@ -10,7 +10,9 @@
  * @return {boolean}
  */
 var isPowerOfTwo = function (n) {
-  // 2.Math.log2(n)
-  return Number.isInteger(Math.log2(n));
+  // 3.位运算：利用 n & n - 1 打掉最后一位 1
+  // 2的幂特点：转为二进制 只有一个 1
+  // 如： 16 => 10000  1 => 1  4 => 100
+  return n > 0 && (n & (n - 1)) === 0;
 };
 // @lc code=end
