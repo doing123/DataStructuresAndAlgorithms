f3d87a07c37b0feee67db60fe491456ce35197ad
junshan qin <qinjunshan123@gmail.com>
2020-10-26 21:24:48 +0800

    120.三角形最小路径和.js

--- 120.三角形最小路径和.js
+++ 120.三角形最小路径和.js
@@ -12,21 +12,14 @@
 var minimumTotal = function (triangle) {
   // 2.从底层向上层遍历
   let height = triangle.length;
-  const dp = new Array(height); // 二维数组
-  for (let i = 0; i < height; i++) {
-    dp[i] = new Array(i + 1);
-  }
 
   // 从后往前遍历处理：从死到生
-  for (let i = height - 1; i >= 0; i--) {
+  const dp = [...triangle[height - 1]]; // 一维数组
+  for (let i = height - 2; i >= 0; i--) {
     for (let j = 0; j < triangle[i].length; j++) {
-      if (i === height - 1) {
-        dp[i][j] = triangle[i][j];
-      } else {
-        dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j];
-      }
+      dp[j] = Math.min(dp[j], dp[j + 1]) + triangle[i][j];
     }
   }
-  return dp[0][0];
+  return dp[0];
 };
 // @lc code=end
