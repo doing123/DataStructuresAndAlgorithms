84f051e3d0804ed732c73b3bdc0445e5e1def2fb
junshan qin <qinjunshan123@gmail.com>
2020-10-26 22:11:13 +0800

    122.买卖股票的最佳时机-ii.js

--- 122.买卖股票的最佳时机-ii.js
+++ 122.买卖股票的最佳时机-ii.js
@@ -10,39 +10,11 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  // 1.上涨区间，只买卖一次，省手续费哈
-  // let result = 0;
-  // let start = 0;
-  // for (let i = 1, len = prices.length; i < len; i++) {
-  //   if (prices[i] < prices[i - 1]) {
-  //     start = i;
-  //   }
-  //   if (prices[i] > prices[i + 1] || i == len - 1) {
-  //     result += prices[i] - prices[start];
-  //   }
-  // }
-  // return result;
-
-  // 2.优化
-  // let result = 0;
-  // for (let i = 1, len = prices.length; i < len; i++) {
-  //   if (prices[i] > prices[i - 1]) {
-  //     result += prices[i] - prices[i - 1];
-  //   }
-  // }
-  // return result;
-
-  // 3.
+  // 1.循环：线性连续遍历
   let result = 0;
-  for (let i = 1, len = prices.length; i < len; i++) {
-    for (let j = i; j < len; j++) {
-      if (prices[j] > prices[j - 1]) {
-        result += prices[j] - prices[j - 1];
-        i = j;
-      } else {
-        i = j;
-        break;
-      }
+  for (let i = 1; i < prices.length; i++) {
+    if (prices[i] > prices[i - 1]) {
+      result += prices[i] - prices[i - 1];
     }
   }
   return result;
