471d7e70e954ae076a840107c8effd1496989150
junshan qin <qinjunshan123@gmail.com>
2020-10-26 21:50:27 +0800

    152.乘积最大子数组.js

--- 152.乘积最大子数组.js
+++ 152.乘积最大子数组.js
@@ -10,18 +10,23 @@
  * @return {number}
  */
 var maxProduct = function (nums) {
-  // 1.循环
+  // 2.dp
+  let len = nums.length;
+  const dp = Array.from({ length: len }, () => {
+    return new Array(2).fill(0); // 长度为2，0位保存最大值，1保存最小值
+  });
+  dp[0][0] = nums[0]; // 保存最大值
+  dp[0][1] = nums[0]; // 最小
   let result = nums[0];
-  let prevMax = nums[0];
-  let prevMin = nums[0];
   let temp1, temp2;
   for (let i = 1; i < nums.length; i++) {
-    temp1 = prevMax * nums[i];
-    temp2 = prevMin * nums[i];
+    let num = nums[i];
+    temp1 = dp[i - 1][0] * num;
+    temp2 = dp[i - 1][1] * num;
     // 对于i来说，i - 1的最优解可能是最大值，也可能是最小值
-    prevMin = Math.min(temp1, temp2, nums[i]);
-    prevMax = Math.max(temp1, temp2, nums[i]);
-    result = Math.max(result, prevMax);
+    dp[i][1] = Math.min(temp1, temp2, nums[i]);
+    dp[i][0] = Math.max(temp1, temp2, nums[i]);
+    result = Math.max(result, dp[i][0]);
   }
 
   return result;
