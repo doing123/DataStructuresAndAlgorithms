162f2224e805b015f65c556a8f331e810cccf74f
junshan qin <qinjunshan123@gmail.com>
2020-10-26 22:03:32 +0800

    121.买卖股票的最佳时机.js

--- 121.买卖股票的最佳时机.js
+++ 121.买卖股票的最佳时机.js
@@ -10,26 +10,14 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  // 双循环，暴力破解
-  // let max = 0;
-  // for (let i = 0, len = prices.length; i < len; i++) {
-  //   for (let j = i + 1; j < len; j++) {
-  //     if (prices[j] > prices[i]) {
-  //       max = Math.max(max, prices[j] - prices[i]);
-  //     }
-  //   }
-  // }
-  // return max;
-
-  let len = prices.length;
-  if (len == 0) return 0;
+  // 1.双循环，暴力求解
   let max = 0;
-  let min = prices[0];
-  for (let i = 1; i < len; i++) {
-    max = Math.max(max, prices[i] - min);
-    min = Math.min(min, prices[i]);
+  let len = prices.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i; j < len; j++) {
+      max = Math.max(max, prices[j] - prices[i]);
+    }
   }
-
   return max;
 };
 // @lc code=end
