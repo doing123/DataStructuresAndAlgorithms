d22a7f5a72912cad78b513870adebf2b753038f7
junshan qin <qinjunshan123@gmail.com>
2020-10-28 22:57:17 +0800

    300.最长上升子序列.js

--- 300.最长上升子序列.js
+++ 300.最长上升子序列.js
@@ -10,29 +10,21 @@
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
-  // 2.二分查找
+  // 1.动态规划
   let len = nums.length;
   if (len <= 1) return len;
-  let tail = []; // 递增子序列
-  tail.push(nums[0]);
+  let dp = new Array(len).fill(1);
   for (let i = 0; i < len; i++) {
-    if (nums[i] > tail[tail.length - 1]) {
-      tail.push(nums[i]);
-    } else {
-      // 二分查找递增子序列 tail 中 第一个大于当前值 nums[i] 的元素，替换为当前值
-      let left = 0;
-      let right = tail.length - 1;
-      while (left < right) {
-        let mid = (left + right) >>> 1;
-        if (tail[mid] < nums[i]) {
-          left = mid + 1;
-        } else {
-          right = mid;
-        }
+    // 状态转移方程：
+    // 定义状态要有两个意识：1.最后一步是什么；2.化为子问题
+    // 以nums[i]结尾，不断的去找前面比自己小的
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
       }
-      tail[left] = nums[i];
     }
   }
-  return tail.length;
+
+  return Math.max(...dp);
 };
 // @lc code=end
