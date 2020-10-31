3463fb0759d21d210de0acf6ed336616c7f8f350
junshan qin <qinjunshan123@gmail.com>
2020-10-31 15:56:39 +0800

    200.岛屿数量.js

--- 200.岛屿数量.js
+++ 200.岛屿数量.js
@@ -11,10 +11,13 @@
  */
 var numIslands = function (grid) {
   // 1.DFS 深度优先搜索 + 改变颜色
+  // 优化
   if (!grid.length || !grid[0].length) return 0;
   let result = 0;
   let row = grid.length;
   let col = grid[0].length;
+  let x = [1, -1, 0, 0];
+  let y = [0, 0, 1, -1];
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
       if (grid[i][j] === '1') {
@@ -30,10 +33,9 @@ var numIslands = function (grid) {
       return;
     }
     grid[i][j] = '0';
-    helper(i - 1, j);
-    helper(i + 1, j);
-    helper(i, j - 1);
-    helper(i, j + 1);
+    for (let k = 0; k < 4; k++) {
+      helper(i + x[k], j + y[k]);
+    }
   }
 };
 // @lc code=end
