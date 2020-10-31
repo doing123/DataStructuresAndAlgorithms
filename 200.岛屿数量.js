7adb1def4b49b6c6a8de63b38c20ae2a871a9984
junshan qin <qinjunshan123@gmail.com>
2020-10-31 17:51:36 +0800

    200.岛屿数量.js

--- 200.岛屿数量.js
+++ 200.岛屿数量.js
@@ -10,23 +10,14 @@
  * @return {number}
  */
 var numIslands = function (grid) {
-  // 3.并查集 TODO 没吃透
+  // 4.并查集 另一种写法
   if (!grid.length || !grid[0].length) return 0;
   let row = grid.length;
   let col = grid[0].length;
   let x = [1, -1, 0, 0];
   let y = [0, 0, 1, -1];
-  let result = 0;
-  let parents = new Array(row * col);
-
-  for (let i = 0; i < row; i++) {
-    for (let j = 0; j < col; j++) {
-      if (grid[i][j] === '1') {
-        result++;
-        parents[i * col + j] = i * col + j; // parent指向自身
-      }
-    }
-  }
+  const dummy = row * col + 1;
+  const uf = new UnionFind(dummy);
 
   for (let i = 0; i < row; i++) {
     for (let j = 0; j < col; j++) {
@@ -35,29 +26,64 @@ var numIslands = function (grid) {
           let m = i + x[k];
           let n = j + y[k];
           if (m >= 0 && m < row && n >= 0 && n < col && grid[m][n] === '1') {
-            union(i * col + j, m * col + n);
+            uf.union(i * col + j, m * col + n);
           }
         }
+      } else if (grid[i][j] === '0') {
+        uf.union(i * col + j, dummy);
       }
     }
   }
 
-  return result;
-
-  function union (p, q) {
-    let rootP = find(p);
-    let rootQ = find(q);
-    if (rootQ === rootP) return;
-    parents[rootP] = rootQ;
-    result--;
-  }
-
-  function find (i) {
-    while (i !== parents[i]) {
-      parents[i] = parents[parents[i]];
-      i = parents[i];
-    }
-    return i;
-  }
+  return uf.getCount() - 1; // 返回并查集的个数减一
 };
+
+class UnionFind {
+  // 构造一个节点数为 n 的集合
+  constructor (n) {
+    this.count = n;
+    this.parent = new Array(n);
+    this.size = new Array(n); // size数组记录着每棵树的重量
+    for (let i = 0; i < n; i++) {
+      this.parent[i] = i; // parent指向自身
+      this.size[i] = 1;
+    }
+  }
+
+  // 连通节点 p 和 q, p 和 q 都是索引
+  union (p, q) {
+    let rootP = this.find(p);
+    let rootQ = this.find(q);
+    if (rootP === rootQ) return;
+    // 小树接到大树下面，这样较平衡
+    if (this.size[rootP] > this.size[rootQ]) {
+      this.parent[rootQ] = rootP;
+      this.size[rootP] += this.size[rootQ];
+    } else {
+      this.parent[rootP] = rootQ;
+      this.size[rootQ] += this.size[rootP];
+    }
+    this.count--;
+  }
+
+  // 判断 p、q 是否连通
+  isConnected (p, q) {
+    return this.find(p) === this.find(q);
+  }
+
+  // 找到 x 节点的 root
+  find (x) {
+    while (this.parent[x] !== x) {
+      // 进行路径压缩
+      this.parent[x] = this.parent[this.parent[x]];
+      x = this.parent[x];
+    }
+    return x;
+  }
+
+  // 返回节点数
+  getCount () {
+    return this.count;
+  }
+}
 // @lc code=end
