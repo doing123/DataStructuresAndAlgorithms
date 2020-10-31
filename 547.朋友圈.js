2703a74a86f7a078734957cef7a4f6c8a7ba4650
junshan qin <qinjunshan123@gmail.com>
2020-10-31 11:45:26 +0800

    547.朋友圈.js

--- 547.朋友圈.js
+++ 547.朋友圈.js
@@ -12,12 +12,12 @@
 var findCircleNum = function (M) {
   // 2.并查集
 
-  // 初始状态：每个学生都没有被访问
   let result = M.length;
+  // 初始状态：每个学生的parent 都指向自身
   const parents = Array.from({ length: result }).map((item, index) => index);
   for (let i = 0; i < M.length; i++) {
     for (let j = 0; j < M[i].length; j++) {
-      if (M[i][j]) {
+      if (M[i][j] === 1) {
         union(i, j);
       }
     }
@@ -25,13 +25,16 @@ var findCircleNum = function (M) {
   return result;
 
   function union (i, j) {
+    // 属于同一个子集
     if (find(i) === find(j)) {
       return;
     }
+    // 属于不同子集，将两个自己合并，result--
     parents[parents[i]] = parents[j];
     result--;
   }
 
+  // 并查集常规搜索，添加路径压缩
   function find (x) {
     if (parents[x] === x) return x;
     return (parents[x] = find(parents[x]));
