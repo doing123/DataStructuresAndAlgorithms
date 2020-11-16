5703cf80cc853b958859310c435c29b7ef8b7cee
junshan qin <qinjunshan123@gmail.com>
2020-11-17 00:02:35 +0800

    617.合并二叉树.js

--- 617.合并二叉树.js
+++ 617.合并二叉树.js
@@ -18,15 +18,14 @@
  * @return {TreeNode}
  */
 var mergeTrees = function (t1, t2) {
-  // 1.原树上修改
+  // 2.生成新树
   if (!t1 && t2) return t2;
   if ((!t1 && !t2) || (t1 && !t2)) return t1;
 
-  t1.val += t2.val;
+  const root = new TreeNode(t1.val + t2.val);
+  root.left = mergeTrees(t1.left, t2.left);
+  root.right = mergeTrees(t1.right, t2.right);
 
-  t1.left = mergeTrees(t1.left, t2.left);
-  t1.right = mergeTrees(t1.right, t2.right);
-
-  return t1;
+  return root;
 };
 // @lc code=end
