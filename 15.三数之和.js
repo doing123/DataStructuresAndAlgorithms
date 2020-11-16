61106033029a38fc78eb668b4ce5f3facf2bc200
junshan qin <qinjunshan123@gmail.com>
2020-11-16 23:24:38 +0800

    15.三数之和.js

--- 15.三数之和.js
+++ 15.三数之和.js
@@ -15,18 +15,21 @@ var threeSum = function (nums) {
   let len = nums.length;
   nums.sort((a, b) => a - b);
   for (let i = 0; i < len - 2; i++) {
+    let n1 = nums[i];
     if (nums[i] > 0) break;
     if (nums[i] === nums[i - 1]) continue; // 去重
 
     let left = i + 1;
     let right = len - 1;
     while (left < right) {
-      let sum = nums[i] + nums[left] + nums[right];
+      let n2 = nums[left];
+      let n3 = nums[right];
+      let sum = n1 + n2 + n3;
       if (sum === 0) {
-        result.push([nums[i], nums[left++], nums[right--]]);
+        result.push([n1, n2, n3]);
         // 去重
-        while (nums[left] === nums[left - 1]) left++;
-        while (nums[right] === nums[right + 1]) right--;
+        while (left < right && nums[left] === n2) left++;
+        while (left < right && nums[right] === n3) right--;
       } else if (sum < 0) {
         left++;
       } else {
