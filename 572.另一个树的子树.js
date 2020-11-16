2ea8a4b20237fc1ea79d582c126c17c7a91d9fb3
junshan qin <qinjunshan123@gmail.com>
2020-11-16 23:35:00 +0800

    572.另一个树的子树.js

---
+++ 572.另一个树的子树.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=572 lang=javascript
+ *
+ * [572] 另一个树的子树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} s
+ * @param {TreeNode} t
+ * @return {boolean}
+ */
+var isSubtree = function (s, t) {
+  return JSON.stringify(s).indexOf(JSON.stringify(t)) > -1;
+};
+// @lc code=end
