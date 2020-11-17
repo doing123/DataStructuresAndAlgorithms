7d8ef18f523a0f99d9898af73ccbfeba06da000f
junshan qin <qinjunshan123@gmail.com>
2020-11-18 00:33:50 +0800

    482.密钥格式化.js

--- 482.密钥格式化.js
+++ 482.密钥格式化.js
@@ -11,18 +11,22 @@
  * @return {string}
  */
 var licenseKeyFormatting = function (S, K) {
-  // 2.数组 unshift
-  S = S.toUpperCase().split('-').join('');
+  // 3.顺序插入
+  S = S.toUpperCase().replace(/-/g, '');
+  let index = S.length % K;
+  let s1 = S.slice(0, index);
+  let s2 = S.slice(index);
+  let count = s2.length / K;
+
   let result = [];
-  while (S.length) {
-    if (S.length > K) {
-      result.unshift(S.slice(-K));
-      S = S.slice(0, S.length - K);
-    } else {
-      result.unshift(S);
-      S = '';
-    }
+  for (let i = 0; i < count; i++) {
+    // result.push(s2.substr(i * K, K)); // substr: 从起始索引号提取字符串中指定数目的字符。
+    result.push(s2.substring(i * K, (i + 1) * K)); // substring: 提取字符串中两个指定的索引号之间的字符。
   }
+  if (s1) {
+    result.unshift(s1);
+  }
+
   return result.join('-');
 };
 // @lc code=end
