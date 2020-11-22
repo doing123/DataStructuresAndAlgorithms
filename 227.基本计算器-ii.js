e19d6f155b92e61a4096aefda87c7fcbb871a2c4
junshan qin <qinjunshan123@gmail.com>
2020-11-23 00:28:57 +0800

    227.基本计算器-ii.js

--- 227.基本计算器-ii.js
+++ 227.基本计算器-ii.js
@@ -10,92 +10,37 @@
  * @return {number}
  */
 var calculate = function (s) {
-  // 1.逆波兰式  // TODO 最后一个用例不通
-  let stack = []; // 运算符
-  let queue = []; // 数字
-  let i = 0;
-  let len = s.length;
-  let result = 0;
-
-  while (i < len) {
-    let c = s.charAt(i);
-    if (c === ' ') i++;
-    else if (['+', '-'].includes(c)) {
-      // 栈顶元素为平级，先弹出再入栈
-      while (
-        stack.length &&
-        ['*', '/', '+', '-'].includes(stack[stack.length - 1])
-      ) {
-        queue.push(stack.pop());
+  // 1.利用队列
+  let queue = [];
+  let n = '';
+  let sign = '+';
+  for (let i = 0; i <= s.length; i++) {
+    if (s[i] === ' ') continue;
+    if (/\D/.test(s[i])) {
+      switch (sign) {
+        case '+':
+          queue.push(n);
+          break;
+        case '-':
+          queue.push(-n);
+          break;
+        case '*':
+          queue.push(queue.pop() * n);
+          break;
+        case '/':
+          queue.push(queue.pop() / n | 0); // 取整
+          break;
+        default:
+          break;
       }
-      stack.push(c);
-      i++;
-    } else if (['*', '/'].includes(c)) {
-      // 栈顶元素为平级，先弹出再入栈
-      while (stack.length && ['*', '/'].includes(stack[stack.length - 1])) {
-        queue.push(stack.pop());
-      }
-      stack.push(c);
-      i++;
-    } else if (c === '(') {
-      stack.push(c);
-      i++;
-    } else if (c === ')') {
-      // 弹出栈顶元素，直到 '('
-      while (stack.length && stack[stack.length - 1] !== '(') {
-        queue.push(stack.pop());
-      }
-      stack.pop();
-      i++;
+      sign = s[i];
+      n = '';
     } else {
-      let tmp = c;
-      i++;
-      while (i < len && isNumber(s.charAt(i))) {
-        tmp += s.charAt(i);
-        i++;
-      }
-      queue.push(Number(tmp));
+      n += s[i];
     }
   }
 
-  while (stack.length) {
-    queue.push(stack.pop())
-  }
-
-  // 第二步，计算
-  let evalStack = [];
-  while (queue.length) {
-    let item = queue.shift();
-    switch (item) {
-      case '+':
-        result = evalStack.pop() + evalStack.pop();
-        break;
-      case '*':
-        result = evalStack.pop() * evalStack.pop();
-        break;
-      case '-':
-        let last = evalStack.pop();
-        result = evalStack.pop() - last;
-        break;
-      case '/':
-        let last2 = evalStack.pop();
-        result = parseInt(evalStack.pop() / last2);
-        break;
-      default:
-        result = item;
-        break;
-    }
-
-    evalStack.push(result);
-  }
-
-  return result;
-
-  function isNumber (c) {
-    c = Number(c);
-    return typeof c === 'number' && !isNaN(c);
-  }
+  return queue.reduce((prev, val) => prev + (val | 0), 0)
 };
 
-// calculate(' 3/2 ');
 // @lc code=end
