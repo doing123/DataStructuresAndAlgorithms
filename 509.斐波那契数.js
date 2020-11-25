f8149f6f28bb0ee0352f53ca7317999cb6e46573
junshan qin <qinjunshan123@gmail.com>
2020-11-25 23:06:20 +0800

    509.斐波那契数.js

--- 509.斐波那契数.js
+++ 509.斐波那契数.js
@@ -10,18 +10,13 @@
  * @return {number}
  */
 var fib = function (N) {
-  // 2.备忘录 自动向下
+  // 3.dp 数组的迭代解法：自底向上
   if (N === 0) return 0;
-  const memory = [];
-
-  return helper(N);
-
-  function helper (N) {
-    if (N === 1 || N === 2) return 1;
-    if (!memory[N]) {
-      memory[N] = helper(N - 1) + helper(N - 2);
-    }
-    return memory[N];
+  const dp = new Array(N + 1).fill(0);
+  dp[1] = dp[2] = 1;
+  for (let i = 3; i <= N; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
   }
+  return dp[N];
 };
 // @lc code=end
