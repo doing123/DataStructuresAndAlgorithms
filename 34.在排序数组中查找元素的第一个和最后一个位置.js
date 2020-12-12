e6a51c75ef84617536ccb51528e74f7ff5fdc950
junshan qin <qinjunshan123@gmail.com>
2020-12-12 15:20:12 +0800

    34.在排序数组中查找元素的第一个和最后一个位置.js

---
+++ 34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=javascript
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  let left = leftBound(nums, target);
+  let right = rightBound(nums, target);
+  return [left, right];
+};
+
+function leftBound (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = (left + right) >>> 1;
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] === target) {
+      right = mid - 1; // 收缩右边界
+    }
+  }
+  if (nums[left] !== target) return -1;
+  return left;
+}
+
+function rightBound (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = (left + right) >>> 1;
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else if (nums[mid] === target) {
+      left = mid + 1; // 收缩左边界
+    }
+  }
+  if (nums[right] !== target) return -1;
+  return right;
+}
+// @lc code=end
