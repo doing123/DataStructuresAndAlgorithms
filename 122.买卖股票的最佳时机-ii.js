1b8e9f1697b8e021c1cef53d70fc53a4e6d1f6fb
junshan qin <qinjunshan123@gmail.com>
2020-12-21 21:42:52 +0800

    122.买卖股票的最佳时机-ii.js

--- 122.买卖股票的最佳时机-ii.js
+++ 122.买卖股票的最佳时机-ii.js
@@ -10,11 +10,16 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  // 1.线性查找
+  // 3.递归：无穷 for 的情况就是递归的暗示 TODO 最后三个用例不过
   let result = 0;
-  for (let i = 1, len = prices.length; i < len; i++) {
-    if (prices[i] > prices[i - 1]) {
-      result += prices[i] - prices[i - 1]
+  // 买
+  for (let i = 0, len = prices.length; i < len; i++) {
+    // 卖
+    for (let j = i + 1; j < len; j++) {
+      result = Math.max(
+        result,
+        maxProfit(prices.slice(j + 1)) + prices[j] - prices[i] // 子集 + 当前买卖股票的收益
+      );
     }
   }
 
