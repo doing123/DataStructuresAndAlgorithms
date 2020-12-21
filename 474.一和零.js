05fd46dce3d88840538a24789f121cf7ab33bee1
junshan qin <qinjunshan123@gmail.com>
2020-12-21 23:35:43 +0800

    474.一和零.js

--- 474.一和零.js
+++ 474.一和零.js
@@ -12,23 +12,16 @@
  * @return {number}
  */
 var findMaxForm = function (strs, m, n) {
-  // 1.递归 + 备忘录
-  let len = strs.length;
-  let map = new Map();
+  // 2.动态规划
+  // dp[i][j]：表示有 i 个0和 j 个1时能组成的最多字符串个数
+  let dp = Array.from({ length: m + 1 }, () => {
+    return new Array(n + 1).fill(0);
+  });
 
-  return dp(0, m, n, 0);
-
-  function dp (index, m, n, count) {
-    let key = `${index},${m},${n},${count}`;
-    if (map.has(key)) return map.get(key);
-    if (index === len) {
-      map.set(key, count);
-      return count;
-    }
-
+  for (let k = 0, len = strs.length; k < len; k++) {
     let zeroes = 0;
     let ones = 0;
-    for (let c of strs[index]) {
+    for (let c of strs[k]) {
       if (c === '0') {
         zeroes++;
       } else {
@@ -36,19 +29,14 @@ var findMaxForm = function (strs, m, n) {
       }
     }
 
-    let result = 0;
-    // 可以选择当前，可以选或不选
-    if (zeroes <= m && ones <= n) {
-      result = Math.max(
-        dp(index + 1, m, n, count),
-        dp(index + 1, m - zeroes, n - ones, count + 1)
-      );
-    } else {
-      // 剩余 0 或 1 不能满足选择当前项
-      result = dp(index + 1, m, n, count);
+    for (let i = m; i >= zeroes; i--) {
+      for (let j = n; j >= ones; j--) {
+        // 使用 strs 当前项，计数加 1，取最大值
+        dp[i][j] = Math.max(dp[i][j], dp[i - zeroes][j - ones] + 1);
+      }
     }
-    map.set(key, result);
-    return result;
   }
+
+  return dp[m][n];
 };
 // @lc code=end
