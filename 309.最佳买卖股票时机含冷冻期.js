24718b54e3136568d439af29f0582d5c173fe9e3
junshan qin <qinjunshan123@gmail.com>
2020-12-21 23:00:23 +0800

    309.最佳买卖股票时机含冷冻期.js

--- 309.最佳买卖股票时机含冷冻期.js
+++ 309.最佳买卖股票时机含冷冻期.js
@@ -10,23 +10,26 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  // 1.递归 + 备忘录
+  // 2.动态规划
   let len = prices.length;
-  let memo = [];
-  return dp(0);
+  if (len <= 1) return 0;
+  let dp = Array.from({ length: len }, () => {
+    return new Array(2); // 0 空仓；1 持有
+  });
 
-  function dp (start) {
-    if (start > len) return 0;
-    if (memo[start]) return memo[start];
+  // base case
+  dp[0][0] = 0;
+  dp[0][1] = 0 - prices[0]; // 第一天持有，即买入
+  dp[1][0] = Math.max(0, -prices[0] + prices[1]); // 第二天 空仓
+  dp[1][1] = Math.max(-prices[0], -prices[1]); // 第二天 持有
 
-    let min = prices[start];
-    let result = 0;
-    for (let sell = start + 1; sell < len; sell++) {
-      min = Math.min(min, prices[sell]);
-      result = Math.max(result, dp(sell + 2) + prices[sell] - min); // 冷冻期为 1 天，即隔一天才能操作
-    }
-    memo[start] = result;
-    return result;
+  for (let i = 2; i < len; i++) {
+    // 空仓： 前一天空仓，今天不买；前一天持有，今天卖
+    dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+    // 持有：前一天持有，今天不卖；前两天空仓，今天买
+    dp[i][1] = Math.max(dp[i - 1][1], dp[i - 2][0] - prices[i]);
   }
+
+  return dp[len - 1][0];
 };
 // @lc code=end
