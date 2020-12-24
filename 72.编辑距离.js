a410a0d51a5502d34a4ae6a50d580d78921b0586
junshan qin <qinjunshan123@gmail.com>
2020-12-24 20:08:36 +0800

    72.编辑距离.js

--- 72.编辑距离.js
+++ 72.编辑距离.js
@@ -11,27 +11,33 @@
  * @return {number}
  */
 var minDistance = function (word1, word2) {
-  // 2.递归 + 备忘录
+  // 1.dp
   let m = word1.length;
   let n = word2.length;
-  let map = new Map();
-  return dp(m - 1, n - 1);
+  let dp = Array.from({ length: m + 1 }, () => {
+    return new Array(n + 1).fill(0);
+  });
 
-  function dp (i, j) {
-    if (i < 0) return j + 1; // 一个走完，直接返回另一个字符串剩余的长度
-    if (j < 0) return i + 1;
-    let key = `${i},${j}`;
-    if (map.has(key)) return map.get(key);
-    let result;
-    if (word1[i] === word2[j]) {
-      result = dp(i - 1, j - 1);
-    } else {
-      // 取插入、删除、替换三者的最小值
-      result = Math.min(dp(i, j - 1), dp(i - 1, j), dp(i - 1, j - 1)) + 1;
-    }
-
-    map.set(key, result);
-    return result;
+  // base case dp[i][j]：s1[0..i] 和 s2[0..j] 的最小编辑距离
+  for (let i = 0; i <= m; i++) {
+    dp[i][0] = i; // j 为 0， 即只有一种操作：逐个删除
   }
+
+  for (let i = 0; i <= n; i++) {
+    dp[0][i] = i;
+  }
+
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      // dp[i][j] word1前i个字符、word2前j个字符相等时最小步数
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1;
+      }
+    }
+  }
+
+  return dp[m][n];
 };
 // @lc code=end
