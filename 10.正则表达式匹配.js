da1de4e3c839aa71d1f6c914b53e4ed985c50cfd
junshan qin <qinjunshan123@gmail.com>
2020-12-28 09:52:21 +0800

    10.正则表达式匹配.js

--- 10.正则表达式匹配.js
+++ 10.正则表达式匹配.js
@@ -11,28 +11,22 @@
  * @return {boolean}
  */
 var isMatch = function (s, p) {
-  if (s == null || p == null) return false;
-  let len1 = s.length,
-    len2 = p.length;
-  let dp = Array.apply(null, { length: len1 + 1 }).map(() => {
-    return new Array(len2 + 1).fill(false);  // dp[i][j]：表示 s 的前i个字符是否与 p的前j个字符是否匹配
-  });
+  // 2.递归
+  let len1 = s.length;
+  let len2 = p.length;
+  return dp(0, 0);
 
-  dp[0][0] = true;// 初始化 s、p 都为空字符串为 true
-  for (let j = 2; j < len2 + 1; j++) {
-    if (p[j - 1] == '*') dp[0][j] = dp[0][j - 2];
-  }
-  for (let i = 1; i < len1 + 1; i++) {
-    for (let j = 1; j < len2 + 1; j++) {
-      if (s[i - 1] == p[j - 1] || p[j - 1] == '.') {
-        dp[i][j] = dp[i - 1][j - 1];
-      } else if (p[j - 1] == '*') {
-        if (s[i - 1] == p[j - 2] || p[j - 2] == '.')
-          dp[i][j] = dp[i][j - 2] || dp[i - 1][j - 2] || dp[i - 1][j];
-        else dp[i][j] = dp[i][j - 2];
-      }
+  function dp (i, j) {
+    if (j === len2) return i === len1;
+
+    let first = i < len1 && (s[i] === p[j] || p[j] === '.');
+    // 特殊匹配
+    if (j <= len2 - 2 && p[j + 1] === '*') {
+      // 匹配 0 次(跳过 x*) 或 1 次(当前匹配，i加1)
+      return dp(i, j + 2) || (first && dp(i + 1, j));
+    } else {
+      return first && dp(i + 1, j + 1);
     }
   }
-  return dp[len1][len2]; // len1长度的s串 是否匹配 len2长度的p串
 };
 // @lc code=end
