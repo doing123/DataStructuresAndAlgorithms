ccbde88746eef207964a665385a7f83ce4847a7f
junshan qin <qinjunshan123@gmail.com>
2021-02-07 16:16:59 +0800

    297.二叉树的序列化与反序列化.js

--- 297.二叉树的序列化与反序列化.js
+++ 297.二叉树的序列化与反序列化.js
@@ -20,8 +20,22 @@
  * @return {string}
  */
 var serialize = function (root) {
-  // 3.中序遍历 无法实现反序列化，因 根节点 在序列化的中间位置
+  // 4.层级遍历
+  if (!root) return '';
+  let result = [];
+  let queue = [root];
+  while (queue.length) {
+    let first = queue.shift();
+    if (!first) {
+      result.push('#');
+      continue;
+    }
+    result.push(first.val);
+    queue.push(first.left);
+    queue.push(first.right);
+  }
 
+  return result.toString();
 };
 
 /**
@@ -31,7 +45,36 @@ var serialize = function (root) {
  * @return {TreeNode}
  */
 var deserialize = function (data) {
+  if (!data) return null;
+  const nodes = data.split(',');
+  let first = nodes.shift();
+  let root = new TreeNode(+first);
+  let queue = [root];
 
+  while (queue.length) {
+    // 队列中存的都是父节点
+    let parent = queue.shift();
+
+    // 父节点对应的左侧子节点的值
+    let left = nodes.shift();
+    if (left === '#') {
+      parent.left = null;
+    } else {
+      parent.left = new TreeNode(+left);
+      queue.push(parent.left);
+    }
+
+    // 父节点对应的右侧子节点的值
+    let right = nodes.shift();
+    if (right === '#') {
+      parent.right = null;
+    } else {
+      parent.right = new TreeNode(+right);
+      queue.push(parent.right);
+    }
+  }
+
+  return root;
 };
 
 /**
