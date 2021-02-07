fe14fe87fe7e0d6b60055ac459eae08d0fd91ced
junshan qin <qinjunshan123@gmail.com>
2021-02-07 12:38:35 +0800

    105.从前序与中序遍历序列构造二叉树.js

--- 105.从前序与中序遍历序列构造二叉树.js
+++ 105.从前序与中序遍历序列构造二叉树.js
@@ -18,31 +18,17 @@
  * @return {TreeNode}
  */
 var buildTree = function (preorder, inorder) {
-  // preorder[0]: 3 为 head，[1]：9 为 leftTree 的顶点
-  // 中序遍历数组中以 3 为分割点，左边为 leftTree 的数据，右边第为 rightTree 的数据
-  // 1.递归
-  let len = inorder.length;
-  let map = new Map();
-  for (let i = 0; i < len; i++) {
-    map.set(inorder[i], i);
-  }
-
-  return helper(0, len - 1, 0, len - 1);
-
-  // 前序数组左边界、右边界、中序数组左边界、右边界
-  function helper (prevLeft, prevRight, inLeft, inRight) {
+  return build(0, preorder.length - 1, 0, inorder.length - 1);
+  function build (prevLeft, prevRight, inLeft, inRight) {
     if (prevLeft > prevRight) return null;
-    let val = preorder[prevLeft]; // 前序遍历的第一个 为 根节点
-    let index = map.get(val);
+    // 1.在前序遍历数组中确定 root 节点的值
+    let val = preorder[prevLeft];
     let root = new TreeNode(val);
-    let leftTreeLen = index - inLeft;
-    root.left = helper(prevLeft + 1, prevLeft + leftTreeLen, inLeft, index - 1);
-    root.right = helper(
-      prevLeft + leftTreeLen + 1,
-      prevRight,
-      index + 1,
-      inRight
-    );
+    let index = inorder.findIndex((item) => item === val); // 根节点在 中序数组中的位置，左边为左子树，右边为右子树
+    // 2.构建左右子树
+    let leftLen = index - inLeft;
+    root.left = build(prevLeft + 1, prevLeft + leftLen, inLeft, index - 1);
+    root.right = build(prevLeft + leftLen + 1, prevRight, index + 1, inRight);
 
     return root;
   }
