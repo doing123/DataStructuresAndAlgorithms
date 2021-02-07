64e71984c539454bcb0e94641cbd65d8b57bb28d
junshan qin <qinjunshan123@gmail.com>
2021-02-07 10:57:32 +0800

    236.二叉树的最近公共祖先.js

--- 236.二叉树的最近公共祖先.js
+++ 236.二叉树的最近公共祖先.js
@@ -19,12 +19,23 @@
  * @return {TreeNode}
  */
 var lowestCommonAncestor = function (root, p, q) {
-  if (!root || root.val === p.val || root.val === q.val) return root;
+  // 后序遍历
+  // base case
+  if (!root) return null;
+  if (root.val === p.val || root.val === q.val) {
+    return root;
+  }
+
+  // 递归子树：找到 p 或 q 节点
   const left = lowestCommonAncestor(root.left, p, q);
   const right = lowestCommonAncestor(root.right, p, q);
+
+  // 如果p和q都在以 root 为根的树中，那么 left 和 right 一定分别是 p 和 q（从 base case 看出来的）
   if (left && right) {
     return root;
   }
+
+  // 如果 p 和 q 只有一个存在于 root 为根的树中，函数返回该节点。因为 p 或 q 之一存在其子树上
   return left || right;
 };
 // @lc code=end
