5436734ad5a3e282e7f6cc43bb0e233b897e8d7a
junshan qin <qinjunshan123@gmail.com>
2021-02-14 12:03:20 +0800

    496.下一个更大元素-i.js

--- 496.下一个更大元素-i.js
+++ 496.下一个更大元素-i.js
@@ -11,9 +11,14 @@
  * @return {number[]}
  */
 var nextGreaterElement = function (nums1, nums2) {
+  // 2.保存 nums2 中每个值的位置
+  const map = {};
+  for (let i = 0; i < nums2.length; i++) {
+    map[nums2[i]] = i;
+  }
   let result = [];
   for (let val of nums1) {
-    let index = nums2.findIndex((item) => item === val);
+    let index = map[val];
     let flag = true;
     for (let i = index + 1; i < nums2.length; i++) {
       if (nums2[i] > val) {
