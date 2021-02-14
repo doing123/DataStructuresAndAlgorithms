882d6116e2dec7c28c5476f3896bd0e313582716
junshan qin <qinjunshan123@gmail.com>
2021-02-14 12:47:14 +0800

    503.下一个更大元素-ii.js

--- 503.下一个更大元素-ii.js
+++ 503.下一个更大元素-ii.js
@@ -11,18 +11,20 @@
  */
 var nextGreaterElements = function (nums) {
   // 2.数组长度加倍，利用单调栈算法模板
+  // 通过 % 运算符求模（余数），来获得环形特效
   let len = nums.length;
-  let result = new Array(len * 2);
-  const arr = [...nums, ...nums];
+  let result = new Array(len);
   const stack = []; // 保存元素
+
+  // 假如这个数组长度翻倍了
   for (let i = len * 2 - 1; i >= 0; i--) {
-    while (stack.length && stack[stack.length - 1] <= nums[i]) {
+    while (stack.length && stack[stack.length - 1] <= nums[i % len]) {
       stack.pop();
     }
-    result[i] = stack.length ? stack[stack.length - 1] : -1;
-    stack.push(arr[i]);
+    result[i % len] = stack.length ? stack[stack.length - 1] : -1;
+    stack.push(nums[i % len]);
   }
 
-  return result.slice(0, len);
+  return result;
 };
 // @lc code=end
