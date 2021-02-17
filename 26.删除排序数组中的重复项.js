86920257c92395932e7b836dfc2babca67156cd2
junshan qin <qinjunshan123@gmail.com>
2021-02-17 16:47:02 +0800

    26.删除排序数组中的重复项.js

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -11,12 +11,16 @@
  */
 var removeDuplicates = function (nums) {
   let slow = 0;
-  for (let fast = 0; fast < nums.length; fast++) {
+  let fast = 0;
+  while (fast < nums.length) {
     if (nums[slow] !== nums[fast]) {
       slow++;
+      // 维护 nums[0..slow] 无重复
       nums[slow] = nums[fast];
     }
+    fast++;
   }
+  // 数组长度为 slow 索引 + 1
   return slow + 1;
 };
 // @lc code=end
