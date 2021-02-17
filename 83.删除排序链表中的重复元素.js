4725d0cb2dfc156bd73711eeb58c4039d985ff41
junshan qin <qinjunshan123@gmail.com>
2021-02-17 16:56:00 +0800

    83.删除排序链表中的重复元素.js

--- 83.删除排序链表中的重复元素.js
+++ 83.删除排序链表中的重复元素.js
@@ -19,7 +19,7 @@
 var deleteDuplicates = function (head) {
   let node = head;
   while (node && node.next) {
-    if (node.next.val == node.val) {
+    if (node.next.val === node.val) {
       node.next = node.next.next;
     } else {
       node = node.next;
