46cf5d1896ad521b61a38735a949ecebc2fe9673
junshan qin <qinjunshan123@gmail.com>
2021-02-17 16:31:36 +0800

    316.去除重复字母.js

--- 316.去除重复字母.js
+++ 316.去除重复字母.js
@@ -10,30 +10,29 @@
  * @return {string}
  */
 var removeDuplicateLetters = function (s) {
-  let map = {};
+  // 2.使用 256 数组
+  // 因为输入为 ASCII 字符，大小 256 够用了
+  let count = new Array(256).fill(0);
   for (let i = 0; i < s.length; i++) {
-    if (map[s[i]] === undefined) {
-      map[s[i]] = 0;
-    }
-    map[s[i]]++;
+    count[s.charCodeAt(i)]++;
   }
 
   let result = [];
-  let inResult = {};
+  let inResult = new Array(256).fill(false);
   for (let i = 0; i < s.length; i++) {
     // 每遍历过一个字符，都将对应的计数减一
-    map[s[i]]--;
-    if (inResult[s[i]]) continue;
+    count[s.charCodeAt(i)]--;
+    if (inResult[s.charCodeAt(i)]) continue;
 
     while (result.length && result[result.length - 1] > s[i]) {
       // 若之后不存在栈顶元素了，则停止 pop
-      if (map[result[result.length - 1]] === 0) break;
-      let top = result.pop();
-      inResult[top] = false;
+      if (count[result[result.length - 1].charCodeAt()] === 0) break;
+      let top = result.pop(); // ASCII 字符
+      inResult[top.charCodeAt(0)] = false;
     }
 
     result.push(s[i]);
-    inResult[s[i]] = true;
+    inResult[s.charCodeAt(i)] = true;
   }
 
   return result.join('');
